71cd6e1f1008e08dcd10de0a288ba09074afc1c8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-07-24 19:45:19 +0100

    Add examples

---
+++ cucumber-expressions/ruby/spec/cucumber/cucumber_expressions/expression_examples_spec.rb
@@ -0,0 +1,29 @@
+require 'cucumber/cucumber_expressions/cucumber_expression'
+require 'cucumber/cucumber_expressions/regular_expression'
+require 'cucumber/cucumber_expressions/transform_lookup'
+
+module Cucumber
+  module CucumberExpressions
+    describe 'examples.txt' do
+      def match(expression_text, text)
+        expression = expression_text =~ /\/(.*)\// ?
+          RegularExpression.new(Regexp.new($1), TransformLookup.new) :
+          CucumberExpression.new(expression_text, [], TransformLookup.new)
+
+        arguments = expression.match(text)
+        return nil if arguments.nil?
+        arguments.map { |arg| arg.transformed_value }
+      end
+
+      File.open(File.expand_path("../../../../examples.txt", __FILE__), "r:utf-8") do |io|
+        chunks = io.read.split(/^---/m)
+        chunks.each do |chunk|
+          expression_text, text, args = *chunk.strip.split(/\n/m)
+          it "Works with: #{expression_text}" do
+            expect( match(expression_text, text).inspect ).to eq(args)
+          end
+        end
+      end
+    end
+  end
+end
