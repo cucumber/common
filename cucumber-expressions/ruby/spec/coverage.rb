ab931bec52ca7f55411c9fae989552006404c3b1
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-08-05 15:44:08 +0100

    [cucumber-expressions] Fix Ruby build

--- cucumber-expressions/ruby/spec/coverage.rb
+++ cucumber-expressions/ruby/spec/coverage.rb
@@ -2,10 +2,5 @@
 require 'simplecov'
 formatters = [ SimpleCov::Formatter::HTMLFormatter ]
 
-if ENV['TRAVIS']
-  require 'coveralls'
-  formatters << Coveralls::SimpleCov::Formatter
-end
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(*formatters)
 SimpleCov.start
