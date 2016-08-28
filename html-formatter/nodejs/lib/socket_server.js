d44f5d28be0792c3f3beb106e33362afe97c2f6d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-08-28 14:16:19 +0200

    [cucumber-html-formatter] Add debugging

--- html-formatter/nodejs/lib/socket_server.js
+++ html-formatter/nodejs/lib/socket_server.js
@@ -2,14 +2,19 @@ import net from "net"
 import es from "event-stream"
 import FromJsonStream from "./from_json_stream"
 
+import dbg from "debug"
+const debug = dbg('cucumber-html:SocketServer')
+
 class SocketServer {
   constructor(engine) {
     this._engine = engine
   }
 
   start(port, closeEngineStreamOnDisconnect) {
+    debug(`start(${port}, ${closeEngineStreamOnDisconnect})`)
     return new Promise((resolve, reject) => {
       this._server = net.createServer(socket => {
+        debug('New connection')
         const splitStream = es.split() // deliver individual lines
         const fromJsonStream = new FromJsonStream()
         socket.pipe(splitStream).pipe(fromJsonStream).pipe(this._engine.openStream(), {end: closeEngineStreamOnDisconnect})
