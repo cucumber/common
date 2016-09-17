a00fd4304c6cb0bef8839ede7172456166a07c96
charlierudolph <charles.rudolph@originate.com>
2016-09-17 09:18:46 -0700

    [cucumber-expressions] make require-able and add example

---
+++ cucumber-expressions/javascript/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CucumberExpression: require('./lib/cucumber_expression'),
+  CucumberExpressionGenerator: require('./lib/cucumber_expression_generator'),
+  RegularExpression: require('./lib/regular_expression'),
+  Transform: require('./lib/transform'),
+  TransformLookup: require('./lib/transform_lookup')
+};
