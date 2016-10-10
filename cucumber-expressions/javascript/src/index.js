c68a16535937c6893232c59ad7cd78ba1a43b5d0
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-10-10 10:42:08 +0100

    Fix typo in import

--- cucumber-expressions/javascript/src/index.js
+++ cucumber-expressions/javascript/src/index.js
@@ -2,7 +2,7 @@ import CucumberExpression from "./cucumber_expression"
 import RegularExpression from "./regular_expression"
 import CucumberExpressionGenerator from "./cucumber_expression_generator"
 import TransformLookup from "./transform_lookup"
-import Transform from "./Transform"
+import Transform from "./transform"
 
 export {
   CucumberExpression,
