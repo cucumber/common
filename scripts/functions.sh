0925689f1913cfbb42a0df8616591a4f84fe0f4c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-11-01 23:27:05 +0000

    build: Refactor splitsh script

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -2,11 +2,14 @@
 #
 # This bash script defines common functions used by other scripts
 #
-ROOT="$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" ) )" && pwd )"
+function root_dir()
+{
+    echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+}
 
 # Lists all the .travis.yml files in this monorepo. These files declare a manyrepo
 # in the parent directory.
 function travis_ymls()
 {
-    find "${ROOT}" -name "*.travis.yml" -not -path "**/node_modules/*" -not -path "${DIR}/_book/**"
+    find "$(root_dir)" -name "*.travis.yml" -not -path "**/node_modules/*" -not -path "$(root_dir)/_book/**"
 }
