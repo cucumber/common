2ef1d43452e5b85c807e86afeb0e13640960b00d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-01-08 13:23:25 +0000

    Add scripts for git subrepo

---
+++ RELEASE_PROCESS.md
@@ -0,0 +1,48 @@
+# Release Process
+
+The release process is supported by scripts and bash functions in the `scripts` directory.
+To follow the instructions below you first have to load the bash scripts into your
+current shell:
+
+    source ./scripts/functions.sh
+
+## Library groups
+
+Some libraries are implemented in several programming languages.
+These libraries are called _library groups_. Examples are
+_Gherkin_, _Cucumber Expressions_ and _Tag Expressions_.
+
+In the instructions below, `group_path` refers to the main directory of a
+library group, for example `gherkin`, `cucumber-expressions` or `tag-expressions`.
+
+All implementations of a library groups should be released at the same time.
+The reason for this is that they all follow the same versioning scheme
+([semver](http://semver.org/)).
+
+### Preparing the release
+
+Make sure the main `cucumber/cucumber` repo has the most recent commits from
+all of the subrepos:
+
+    pull_subrepos ${group_path}
+
+Make sure they all build successfully:
+
+    # TODO: implement this
+    build_subrepos ${group_path}
+
+Update the version you're about to release. Pay special attention to messages
+printed in blue - you might need to take some manual steps:
+
+    group_update_version ${group_path} ${version}
+
+Commit and push all subrepos
+
+    git commit -am "${group_path}: Release ${version}"
+    git push
+    push_subrepos
+
+Release all the subrepos in the group
+
+    # TODO: finish implementing this
+    group_release "${group_path}""
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,50 +1,302 @@
 #!/usr/bin/env bash
 #
-# This bash script defines common functions used by other scripts
+# This bash script defines common functions used by cucumber contributors
+#
+# TODO: Document with https://github.com/tests-always-included/tomdoc.sh
 #
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 NC='\033[0m'
+
+function echo_err()
+{
+  echo_red "$@" 1>&2
+}
 
 function echo_green
 {
-    echo -e "${GREEN}$1${NC}"
+  echo -e "${GREEN}$@${NC}"
 }
 
 function echo_red
 {
-    echo -e "${RED}$1${NC}"
+  echo -e "${RED}$@${NC}"
+}
+
+function echo_blue
+{
+  echo -e "${BLUE}$@${NC}"
 }
 
 # The root dir of the monorepo
 function root_dir()
 {
-    echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+  echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 }
 
-# Lists all the .travis.yml files in this monorepo (except the one in the root).
-# The presence of a .travis.yml file declares a manyrepo in the parent directory.
-function travis_ymls()
+# Prints all subrepos. Optionally specify a parent dir.
+function subrepos()
 {
-    git ls-files "**/.travis.yml"
+  dir=$1
+  if [ -n "${dir}" ]; then
+    dir="${dir}/"
+  fi
+  git ls-files "${dir}**/.gitrepo" | xargs -n 1 dirname
 }
 
-# Lists all the manyrepos. Each line has two fields (tab separated for cut friendliness)
-# dir and name.
-#
-# For example:
-#
-# cucumber-expressions/java cucumber/cucumber-expressions-java
-function manyrepos()
+# Prints the remote (git URL) of a subrepo
+function subrepo_remote()
 {
-  travis_ymls | while read travis_yml; do
-    manyrepo_name=$(awk '/^#[ \t]+manyrepo:/ {print $3}' ${travis_yml})
-    if [ "${manyrepo_name}" = "" ]
-    then
-      echo -e "${RED}No manyrepo comment in ${travis_yml}${NC}"
-      exit 1
-    fi
-    manyrepo_dir=$(dirname ${travis_yml})
-    echo -e "${manyrepo_dir}\t${manyrepo_name}"
+  subrepo=$1
+  git subrepo status "${subrepo}" | grep "Remote URL:" | sed -e 's/[ \t][ \t]*Remote URL:[ \t][ \t]*//g'
+}
+
+# Updates the version number for all subrepos in a group
+function group_update_version()
+{
+  group_path=$1
+  version=$2
+
+  subrepos "${group_path}" | while read subrepo; do
+    update_version "${subrepo}" "${version}"
   done
+}
+
+function pull_subrepos()
+{
+  subrepos $1 | while read subrepo; do
+    remote=$(subrepo_remote "${subrepo}")
+    echo "pulling ${subrepo} from ${remote}"
+    git subrepo pull "${subrepo}"
+  done
+}
+
+function push_subrepos()
+{
+  subrepos $1 | while read subrepo; do
+    remote=$(subrepo_remote "${subrepo}")
+    echo "pushing ${subrepo} to ${remote}"
+    git subrepo push "${subrepo}"
+  done
+}
+
+function build_subrepos()
+{
+  subrepos $1 | while read subrepo; do
+    pushd "${subrepo}"
+    make
+    popd
+  done
+}
+
+# Releases all implementations of a group.
+#
+# Before running this function you should make sure you are authenticated
+# with all the various package managers:
+#
+#   ./scripts/check-release-karma
+function group_release()
+{
+  group_path=$1
+
+  subrepos "${group_path}" | while read subrepo; do
+    remote=$(subrepo_remote "${subrepo}")
+
+    release_dir="${subrepo}/.release"
+    rm -rf "${release_dir}"
+
+    echo_green "***** Cloning ${remote} (${subrepo}) *****"
+    git clone "${remote}" "${release_dir}"
+    release_subrepo_clone "${subrepo}"
+
+    echo_green "***** Releasing ${remote} *****"
+    pushd "${release_dir}"
+    release_subrepo_clone "${release_dir}"
+    popd
+  done
+}
+
+# Publishes a released package for a subrepo (from a clone of the subrepo)
+function release_subrepo_clone()
+{
+  dir=$1
+
+  ptype=$(project_type "${dir}")
+  if [ -n "${ptype}" ]; then
+    eval ${ptype}_release "${dir}"
+  fi
+}
+
+# Updates the version for a subrepo
+function update_version()
+{
+  subrepo=$1
+  version=$2
+
+  ptype=$(project_type "${subrepo}")
+  if [ -n "${ptype}" ]; then
+    eval ${ptype}_update_version "${subrepo}" "${version}"
+  fi
+}
+
+function project_type()
+{
+  dir=$1
+
+  if [ -f "${dir}/pom.xml" ]; then
+    echo "maven"
+  elif [ -f "${dir}/package.json" ]; then
+    echo "npm"
+  elif [ -f "${dir}/setup.py" ]; then
+    echo "python"
+  elif [ -f "${dir}/cpanfile" ]; then
+    echo "perl"
+  elif [ -f "$(find_path ${dir} "*.gemspec")" ]; then
+    echo "rubygem"
+  elif [ -f "$(find_path ${dir} "*.sln")" ]; then
+    echo "dotnet"
+  elif [ -f "$(find_path ${dir} "*.go")" ]; then
+    echo "go"
+  elif [ -d "$(find_path ${dir} "*.xcodeproj")" ]; then
+    echo "xcode"
+  else
+    echo_err "ERROR: Unrecognised platform: ${subrepo}"
+  fi
+}
+
+function find_path()
+{
+  subrepo=$1
+  glob=$2
+  find "${subrepo}" -maxdepth 1 -name "${glob}" | head -1
+}
+
+################ MAVEN ################
+
+function maven_version()
+{
+  subrepo=$1
+  xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" "${subrepo}/pom.xml"
+}
+
+function maven_update_version()
+{
+  subrepo=$1
+  version=$2
+
+  xmlstarlet ed --inplace --ps -N pom="http://maven.apache.org/POM/4.0.0" \
+    --update "/pom:project/pom:version" \
+    --value "${version}" \
+    "${subrepo}/pom.xml"
+}
+
+function maven_release()
+{
+  dir=$1
+  echo "RELEASING MAVEN ${dir}"
+}
+
+################ NPM ################
+
+function npm_version()
+{
+  subrepo=$1
+  jq -r ".version" "${subrepo}/package.json"
+}
+
+function npm_update_version()
+{
+  subrepo=$1
+  version=$2
+  sed -i "" "s/\(\"version\" *: *\"\)[0-9]*\.[0-9]*\.[0-9]*\(\"\)/\1${version}\2/" "${subrepo}/package.json"
+  echo_green "Updated ${subrepo} to ${version}"
+}
+
+function npm_release() {
+  dir=$1
+  echo "RELEASING NPM ${dir}"
+  # npm install
+  # npm publish
+}
+
+################ RUBYGEM ################
+
+function rubygem_version()
+{
+  subrepo=$1
+  cat "$(find_path "${subrepo}" "*.gemspec")" | grep -m 1 ".version *= *" | sed "s/.*= *'\([^']*\)'.*/\1/"
+}
+
+function rubygem_update_version()
+{
+  subrepo=$1
+  version=$2
+  sed -i "" "s/\(s\.version *= *'\)[0-9]*\.[0-9]*\.[0-9]*\('\)/\1${version}\2/" "$(find_path "${subrepo}" "*.gemspec")"
+  echo_green "Updated ${subrepo} to ${version}"
+}
+
+function rubygem_release() {
+  dir=$1
+  echo "RELEASING GEM ${dir}"
+}
+
+################ PYTHON ################
+
+function python_update_version()
+{
+  subrepo=$1
+  version=$2
+  sed -i "" \
+    -e "s/\(version *= *'\)[0-9]*\.[0-9]*\.[0-9]*\('\)/\1${version}\2/" \
+    -e "s/\(archive\/v\)[0-9]*\.[0-9]*\.[0-9]*\(\.tar\)/\1${version}\2/" \
+    "${subrepo}/setup.py"
+  echo_green "Updated ${subrepo} to ${version}"
+}
+
+function python_release() {
+  dir=$1
+  echo "RELEASING PYTHON ${dir}"
+}
+
+################ PERL ################
+
+function perl_update_version()
+{
+  subrepo=$1
+  version=$2
+  echo "${version}" > "${subrepo}/VERSION"
+  echo_green "Updated ${subrepo} to ${version}"
+}
+
+function perl_release() {
+  dir=$1
+  echo "RELEASING PERL ${dir}"
+}
+
+################ .NET ################
+
+function dotnet_update_version()
+{
+  subrepo=$1
+  version=$2
+  echo_blue "You must manually update to ${version} in ${subrepo}"
+}
+
+################ .NET ################
+
+function go_update_version()
+{
+  subrepo=$1
+  version=$2
+  echo_blue "No need to update to ${version} in ${subrepo} (currently not using a go package manager)"
+}
+
+################ xcode ################
+
+function xcode_update_version()
+{
+  subrepo=$1
+  version=$2
+  echo_blue "No need to update to ${version} in ${subrepo} (currently not using an xcode package manager)"
 }
