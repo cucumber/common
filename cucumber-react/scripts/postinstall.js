28708f68825492ca86bb58348588add222f6a672
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-03-18 20:40:20 +0000

    cucumber-react: Fix the build

--- cucumber-react/scripts/postinstall.js
+++ cucumber-react/scripts/postinstall.js
@@ -2,9 +2,11 @@ var path = require('path')
 var fs = require('fs')
 var exec = require('child_process').exec
 
-fs.exists(path.join(__dirname, '..', 'dist'), function (err) {
-  if (err) throw err;
-  exec('npm run build', {cwd: path.join(__dirname, '..')}, function (err) {
-    if (err) throw err;
-  })
+fs.exists(path.join(__dirname, '..', 'dist'), function (distExists, err) {
+  if (err) throw err
+  if (!distExists) {
+    exec('npm run build', {cwd: path.join(__dirname, '..')}, function (err) {
+      if (err) throw err
+    })
+  }
 })
