2e220ac5a52ef8cefd1978b3fee09905ef42640d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-10-10 23:22:23 +0100

    cucumber-expressions: build tweaks

--- cucumber-expressions/javascript/scripts/postinstall.js
+++ cucumber-expressions/javascript/scripts/postinstall.js
@@ -2,11 +2,9 @@ var path = require('path')
 var fs = require('fs')
 var exec = require('child_process').exec
 
-fs.exists(path.join(__dirname, '..', 'dist'), function(distExists, err) {
-  if (err) throw err
-  if (!distExists) {
-    exec('npm run build', { cwd: path.join(__dirname, '..') }, function(err) {
-      if (err) throw err
-    })
-  }
+fs.access(path.join(__dirname, '..', 'dist'), function(err) {
+  if (!err) return
+  exec('yarn build', { cwd: path.join(__dirname, '..') }, function(err) {
+    if (err) throw err
+  })
 })
