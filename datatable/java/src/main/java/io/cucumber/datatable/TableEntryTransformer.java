670054ca51bb02f6689388b24a75c0860f57f565
M.P. Korstanje <rien.korstanje@gmail.com>
2018-01-13 20:30:40 +0100

    Document TableTransformer interfaces

--- datatable/java/src/main/java/io/cucumber/datatable/TableEntryTransformer.java
+++ datatable/java/src/main/java/io/cucumber/datatable/TableEntryTransformer.java
@@ -2,6 +2,22 @@ package io.cucumber.datatable;
 
 import java.util.Map;
 
+/**
+ * Transforms a table entry to in instance of {@code T}
+ * <p>
+ * A table entry consists of the cells of a row paired
+ * with the header cells.
+ *
+ * @param <T> the target type
+ */
 public interface TableEntryTransformer<T> {
-    T transform(Map<String, String> tableEntry) throws Throwable;
+
+    /**
+     * Transforms a table entry to in instance of {@code T}.
+     *
+     * @param entry a single entry
+     * @return an instance of {@code T}
+     * @throws Throwable when the transform fails for any reason
+     */
+    T transform(Map<String, String> entry) throws Throwable;
 }
