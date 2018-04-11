638c9046937a583ce95c33b0416dc0b5204ae151
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-04-11 10:04:48 +0100

    Add JavaDoc

--- cucumber-expressions/java/src/main/java/io/cucumber/cucumberexpressions/Transformer.java
+++ cucumber-expressions/java/src/main/java/io/cucumber/cucumberexpressions/Transformer.java
@@ -1,5 +1,17 @@
 package io.cucumber.cucumberexpressions;
 
+/**
+ * Transformer for a @{@link ParameterType} with a single capture group.
+ *
+ * @param <T> the type to transform to.
+ */
 public interface Transformer<T> {
+    /**
+     * Transforms a string to an object.
+     *
+     * @param arg the value of the single capture group
+     * @return the transformed object
+     * @throws Throwable if transformation failed
+     */
     T transform(String arg) throws Throwable;
 }
