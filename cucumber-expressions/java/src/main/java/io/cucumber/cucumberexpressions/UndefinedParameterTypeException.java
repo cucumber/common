8d5b876a1555ea1c870f38b5be14033767d44054
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-04-11 22:37:20 +0100

    Remove example code

--- cucumber-expressions/java/src/main/java/io/cucumber/cucumberexpressions/UndefinedParameterTypeException.java
+++ cucumber-expressions/java/src/main/java/io/cucumber/cucumberexpressions/UndefinedParameterTypeException.java
@@ -2,6 +2,6 @@ package io.cucumber.cucumberexpressions;
 
 public class UndefinedParameterTypeException extends CucumberExpressionException {
     UndefinedParameterTypeException(String typeName) {
-        super(String.format("Undefined parameter type {%s}. Please register a ParameterType for {%s}", typeName, typeName));
+        super(String.format("Undefined parameter type {%s}. Please register a ParameterType for {%s}.", typeName, typeName));
     }
 }
