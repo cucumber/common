8599db13b04835d82edbb4a743d37f421a89790a
Thomas Sundberg <tsu@kth.se>
2018-04-20 11:15:37 +0100

    Exposed name and native name to enable Cucumber-JVM to print the language name in English, the native name and the language code.

--- gherkin/java/src/main/java/gherkin/GherkinDialect.java
+++ gherkin/java/src/main/java/gherkin/GherkinDialect.java
@@ -3,22 +3,25 @@ package gherkin;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class GherkinDialect {
-    private final Map<String, List<String>> keywords;
+    private final Map<String, Object> dialect;
     private String language;
 
-    public GherkinDialect(String language, Map<String, List<String>> keywords) {
+    public GherkinDialect(String language, Map<String, Object> dialect) {
         this.language = language;
-        this.keywords = keywords;
+        this.dialect = dialect;
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getFeatureKeywords() {
-        return keywords.get("feature");
+        return (List<String>) dialect.get("feature");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getScenarioKeywords() {
-        return keywords.get("scenario");
+        return (List<String>) dialect.get("scenario");
     }
 
     public List<String> getStepKeywords() {
@@ -31,39 +34,69 @@ public class GherkinDialect {
         return result;
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getBackgroundKeywords() {
-        return keywords.get("background");
+        return (List<String>) dialect.get("background");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getScenarioOutlineKeywords() {
-        return keywords.get("scenarioOutline");
+        return (List<String>) dialect.get("scenarioOutline");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getExamplesKeywords() {
-        return keywords.get("examples");
+        return (List<String>) dialect.get("examples");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getGivenKeywords() {
-        return keywords.get("given");
+        return (List<String>) dialect.get("given");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getWhenKeywords() {
-        return keywords.get("when");
+        return (List<String>) dialect.get("when");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getThenKeywords() {
-        return keywords.get("then");
+        return (List<String>) dialect.get("then");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getAndKeywords() {
-        return keywords.get("and");
+        return (List<String>) dialect.get("and");
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> getButKeywords() {
-        return keywords.get("but");
+        return (List<String>) dialect.get("but");
     }
 
     public String getLanguage() {
         return language;
+    }
+
+    public String getName() {
+        return (String) dialect.get("name");
+    }
+
+    public String getNativeName() {
+        return (String) dialect.get("native");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GherkinDialect that = (GherkinDialect) o;
+        return Objects.equals(dialect, that.dialect) &&
+                Objects.equals(language, that.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dialect, language);
     }
 }
--- gherkin/java/src/main/java/gherkin/IGherkinDialectProvider.java
+++ gherkin/java/src/main/java/gherkin/IGherkinDialectProvider.java
@@ -9,5 +9,11 @@ public interface IGherkinDialectProvider {
 
     GherkinDialect getDialect(String language, Location location);
 
+    List<GherkinDialect> getDialects();
+
+    /**
+     * Use List<GherkinDialect> getDialects() to get all dialects. Extract the language from each dialect.
+     */
+    @Deprecated
     List<String> getLanguages();
 }
