b8604dff4cb38948d1e0b46c39aa86511cc606d7
Len <lennarttange@gmail.com>
2018-05-11 21:34:00 +0200

    made datatable js immutable and expose methods to keep it non-breaking with current cucumber-js

--- datatable/javascript/src/data_table.js
+++ datatable/javascript/src/data_table.js
@@ -7,7 +7,7 @@ class DataTable {
   }
 
   get cells() {
-    return this._rawTable
+    return this._rawTable.slice(0)
   }
 
   get height() {
@@ -33,7 +33,15 @@ class DataTable {
     return this.cells[index]
   }
 
+  /**
+   * The table as a 2-D array, without the first row, unless a range is specified
+   * @param {number} fromRow The first row (including).
+   * @param {number} toRow The last row (excluding).
+   */
   rows(fromRow, toRow) {
+    if (typeof fromRow === 'undefined') {
+      fromRow = 1
+    }
     let rows = this.cells.filter((row, rowIndex) => {
       return rowIndex >= fromRow && (!toRow || rowIndex < toRow)
     })
@@ -46,7 +54,15 @@ class DataTable {
     })
   }
 
+  /**
+   * The table as a 2-D array, without the first column, unless a range is specified
+   * @param {number} fromColumn The first column (including).
+   * @param {number} toColumn The last column (excluding).
+   */
   columns(fromColumn, toColumn) {
+    if (typeof fromColumn === 'undefined') {
+      fromColumn = 1
+    }
     let columns = this.cells.map(row => {
       return row.filter((cell, columnIndex) => {
         return (
@@ -59,6 +75,39 @@ class DataTable {
 
   subTable(fromRow, fromColumn, toRow, toColumn) {
     return this.rows(fromRow, toRow).columns(fromColumn, toColumn)
+  }
+
+  /**
+   * @returns  returns an array of objects where each row is converted to an object (column header is the key)
+   */
+  hashes() {
+    const copy = this.cells
+    const keys = copy[0]
+    const values = copy.slice(1)
+
+    return values.map(row => {
+      const obj = {}
+      keys.forEach((key, i) => {
+        obj[key] = row[i]
+      })
+      return obj
+    }, {})
+  }
+
+  /**
+   * @returns the table as a 2-D array
+   */
+  raw() {
+    return this.cells
+  }
+
+  /**
+   * @returns returns an object where each row corresponds to an entry (first column is the key, second column is the value)
+   */
+  rowHash() {
+    return this.cells.reduce((acc, target) => {
+      return Object.assign(acc, { [target[0]]: target[1] })
+    }, {})
   }
 }
 
--- datatable/javascript/test/data_table_test.js
+++ datatable/javascript/test/data_table_test.js
@@ -5,6 +5,7 @@ const DataTable = require('../src/data_table')
 
 const table2x2 = [['A0', 'A1'], ['B0', 'B1']]
 const table2x3 = [['A0', 'A1', 'A2'], ['B0', 'B1', 'B2']]
+const table3x2 = [['A0', 'A1'], ['B0', 'B1'], ['C0', 'C1']]
 const table3x3 = [['A0', 'A1', 'A2'], ['B0', 'B1', 'B2'], ['C0', 'C1', 'C2']]
 
 describe('DataTable', () => {
@@ -18,6 +19,11 @@ describe('DataTable', () => {
     describe('cells', () => {
       it('returns the 2d array', () => {
         assert.deepEqual(new DataTable(table2x2).cells, table2x2)
+      })
+      it('returns a copy, not the original array', () => {
+        const datatable = new DataTable(table2x2)
+        datatable.cells = 'mutated'
+        assert.deepEqual(datatable.cells, new DataTable(table2x2).cells)
       })
     })
 
@@ -71,9 +77,14 @@ describe('DataTable', () => {
           table3x3[1],
         ])
       })
-
       it('returns all rows from fromRow if no toRow is specified', () => {
         assert.deepEqual(new DataTable(table3x3).rows(1).cells, [
+          table3x3[1],
+          table3x3[2],
+        ])
+      })
+      it('returns all rows without the first one if no fromRow is specified', () => {
+        assert.deepEqual(new DataTable(table3x3).rows().cells, [
           table3x3[1],
           table3x3[2],
         ])
@@ -97,8 +108,15 @@ describe('DataTable', () => {
           [table3x3[2][1]],
         ])
       })
-      it('returns all rows from fromRow if no toRow is specified', () => {
+      it('returns all columns from fromRow if no toColumn is specified', () => {
         assert.deepEqual(new DataTable(table3x3).columns(1).cells, [
+          [table3x3[0][1], table3x3[0][2]],
+          [table3x3[1][1], table3x3[1][2]],
+          [table3x3[2][1], table3x3[2][2]],
+        ])
+      })
+      it('returns all columns without the first one if no fromColumn is specified', () => {
+        assert.deepEqual(new DataTable(table3x3).columns().cells, [
           [table3x3[0][1], table3x3[0][2]],
           [table3x3[1][1], table3x3[1][2]],
           [table3x3[2][1], table3x3[2][2]],
@@ -112,6 +130,38 @@ describe('DataTable', () => {
           [table3x3[1][0], table3x3[1][1]],
           [table3x3[2][0], table3x3[2][1]],
         ])
+      })
+    })
+
+    describe('raw', () => {
+      it('returns the cells', () => {
+        const datatable = new DataTable(table3x3)
+        assert.deepEqual(datatable.raw(), datatable.cells)
+      })
+    })
+
+    describe('hashes', () => {
+      it('returns an array of objects where the keys are the headers', () => {
+        assert.deepEqual(new DataTable(table3x2).hashes(), [
+          {
+            [table3x2[0][0]]: table3x2[1][0],
+            [table3x2[0][1]]: table3x2[1][1],
+          },
+          {
+            [table3x2[0][0]]: table3x2[2][0],
+            [table3x2[0][1]]: table3x2[2][1],
+          },
+        ])
+      })
+    })
+
+    describe('rowHash', () => {
+      it('returns an object where the keys are the first column', () => {
+        assert.deepEqual(new DataTable(table3x2).rowHash(), {
+          [table3x2[0][0]]: table3x2[0][1],
+          [table3x2[1][0]]: table3x2[1][1],
+          [table3x2[2][0]]: table3x2[2][1],
+        })
       })
     })
   })
