d374fd2764b798dd246f8535225d4eac947a9ba4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-07-02 12:20:48 +0100

    messages: Go: Put tests in same package as production code

--- messages/go/messages_test.go
+++ messages/go/messages_test.go
@@ -1,19 +1,18 @@
-package messages_test
+package messages
 
 import (
 	"testing"
 	"github.com/stretchr/testify/require"
 	"github.com/golang/protobuf/proto"
-	"./"
 )
 
 func TestMessages(t *testing.T) {
 	t.Run("builds a pickle doc string", func(t *testing.T) {
-		location := &messages.Location{
+		location := &Location{
 			Line:   uint32(10),
 			Column: uint32(20),
 		}
-		pickleDocString := messages.PickleDocString{
+		pickleDocString := PickleDocString{
 			Location:    location,
 			ContentType: "text/plain",
 			Content:     "some\ncontent\n",
@@ -21,32 +20,32 @@ func TestMessages(t *testing.T) {
 		encoded, err := proto.Marshal(&pickleDocString)
 		require.NoError(t, err)
 		println(len(encoded))
-		var decoded messages.PickleDocString
+		var decoded PickleDocString
 		err = proto.Unmarshal(encoded, &decoded)
 		require.NoError(t, err)
 		require.Equal(t, uint32(20), decoded.Location.Column)
 		require.Equal(t, "some\ncontent\n", decoded.Content)
 	})
 
-	t.Run("builde a step", func(t *testing.T) {
-		location := &messages.Location{
+	t.Run("builds a step", func(t *testing.T) {
+		location := &Location{
 			Line:   10,
 			Column: 20,
 		}
 
-		docString := &messages.DocString{
+		docString := &DocString{
 			Content:  "Hello",
 			Location: location,
 		}
-		step := &messages.Step{
+		step := &Step{
 			Keyword:  "Given",
 			Text:     "the following message:",
-			Argument: &messages.Step_DocString{docString},
+			Argument: &Step_DocString{docString},
 		}
 
 		bytes, err := proto.Marshal(step)
 		require.NoError(t, err)
-		var decoded messages.Step
+		var decoded Step
 		err = proto.Unmarshal(bytes, &decoded)
 		require.NoError(t, err)
 		require.Equal(t, "Hello", decoded.GetDocString().Content)
