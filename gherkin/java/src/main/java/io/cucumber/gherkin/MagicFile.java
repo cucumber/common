8702cfdf9af2409f9a7e1ac28fbfe7edbb123b6a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-07-03 11:54:51 +0100

    gherkin: java: detect more platforms

--- gherkin/java/src/main/java/io/cucumber/gherkin/MagicFile.java
+++ gherkin/java/src/main/java/io/cucumber/gherkin/MagicFile.java
@@ -10,6 +10,7 @@ import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -31,11 +32,18 @@ public class MagicFile {
 
     MagicFile(String executablePattern, Map<Object, Object> props) {
         this.props = props;
-        String formatPattern = executablePattern.replace("{{.OS}}", "%s").replace("{{.Arch}}", "%s");
-        this.fileName = String.format(formatPattern, getOs(), getArch());
+        String formatPattern = executablePattern
+                .replace("{{.OS}}", "%s")
+                .replace("{{.Arch}}", "%s")
+                .replace("{{.Ext}}", "%s");
+        this.fileName = String.format(formatPattern, getOs(), getArch(), getExt());
         File codeFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getFile());
         File targetDir = codeFile.isDirectory() ? codeFile : codeFile.getParentFile();
         targetFile = new File(targetDir, new File(fileName).getName());
+    }
+
+    private String getExt() {
+        return "windows".equals(getOs()) ? ".exe" : "";
     }
 
     public static void deleteOnExit() {
@@ -97,19 +105,119 @@ public class MagicFile {
     }
 
     String getOs() {
-        String osName = ((String) props.get("os.name")).toLowerCase();
-        if (osName.contains("mac")) {
-            return "darwin";
-        }
-        return null;
+        return normalizeOs((String) props.get("os.name"));
     }
 
     String getArch() {
-        String osArch = ((String) props.get("os.arch")).toLowerCase();
-        if (osArch.contains("x86_64")) {
+        return normalizeArch((String) props.get("os.arch"));
+    }
+
+    private static String normalize(String value) {
+        if (value == null) {
+            return "";
+        }
+        return value.toLowerCase(Locale.US).replaceAll("[^a-z0-9]+", "");
+    }
+
+    // https://github.com/trustin/os-maven-plugin/blob/master/src/main/java/kr/motd/maven/os/Detector.java
+    // https://gist.github.com/asukakenji/f15ba7e588ac42795f421b48b8aede63
+    private static String normalizeArch(String value) {
+        value = normalize(value);
+        if (value.matches("^(x8664|amd64|ia32e|em64t|x64)$")) {
+            return "amd64";
+        }
+        if (value.matches("^(x8632|x86|i[3-6]86|ia32|x32)$")) {
             return "386";
         }
-        return null;
+        if (value.matches("^(ia64w?|itanium64)$")) {
+            return "itanium_64"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if ("ia64n".equals(value)) {
+            return "itanium_32"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.matches("^(sparc|sparc32)$")) {
+            return "sparc_32"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.matches("^(sparcv9|sparc64)$")) {
+            return "sparc_64"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.matches("^(arm|arm32)$")) {
+            return "arm";
+        }
+        if ("aarch64".equals(value)) {
+            return "aarch_64"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.matches("^(mips|mips32)$")) {
+            return "mips";
+        }
+        if (value.matches("^(mipsel|mips32el)$")) { // TODO - Is this a typo? Should it be mipsle|mips32le ?
+            return "mipsle";
+        }
+        if ("mips64".equals(value)) {
+            return "mips64";
+        }
+        if ("mips64el".equals(value)) { // TODO - Is this a typo? Should it be mips64le?
+            return "mips64le";
+        }
+        if (value.matches("^(ppc|ppc32)$")) {
+            return "ppc_32"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.matches("^(ppcle|ppc32le)$")) {
+            return "ppcle_32"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if ("ppc64".equals(value)) {
+            return "ppc_64"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if ("ppc64le".equals(value)) {
+            return "ppcle_64"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if ("s390".equals(value)) {
+            return "s390_32"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if ("s390x".equals(value)) {
+            return "s390x";
+        }
+
+        return "unknown";
+    }
+
+    private static String normalizeOs(String value) {
+        value = normalize(value);
+        if (value.startsWith("aix")) {
+            return "aix"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.startsWith("hpux")) {
+            return "hpux"; // TODO - not supported by https://github.com/mitchellh/gox ?
+        }
+        if (value.startsWith("os400")) {
+            // Avoid the names such as os4000
+            if (value.length() <= 5 || !Character.isDigit(value.charAt(5))) {
+                return "os400"; // TODO - not supported by https://github.com/mitchellh/gox ?
+            }
+        }
+        if (value.startsWith("linux")) {
+            return "linux";
+        }
+        if (value.startsWith("macosx") || value.startsWith("osx")) {
+            return "darwin";
+        }
+        if (value.startsWith("freebsd")) {
+            return "freebsd";
+        }
+        if (value.startsWith("openbsd")) {
+            return "openbsd";
+        }
+        if (value.startsWith("netbsd")) {
+            return "netbsd";
+        }
+        if (value.startsWith("solaris") || value.startsWith("sunos")) {
+            return "sunos";
+        }
+        if (value.startsWith("windows")) {
+            return "windows";
+        }
+
+        return "unknown";
     }
 
     String getFileName() {
--- gherkin/java/src/test/java/io/cucumber/gherkin/MagicFileTest.java
+++ gherkin/java/src/test/java/io/cucumber/gherkin/MagicFileTest.java
@@ -29,16 +29,25 @@ public class MagicFileTest {
         }});
 
         assertEquals("darwin", magicFile.getOs());
-        assertEquals("386", magicFile.getArch());
+        assertEquals("amd64", magicFile.getArch());
     }
 
     @Test
-    public void generates_file_name() {
-        MagicFile magicFile = new MagicFile("gherkin-{{.OS}}-{{.Arch}}", new HashMap<Object, Object>() {{
+    public void generates_file_name_for_macos() {
+        MagicFile magicFile = new MagicFile("gherkin-{{.OS}}-{{.Arch}}{{.Ext}}", new HashMap<Object, Object>() {{
             put("os.name", "Mac OS X");
             put("os.arch", "x86_64");
         }});
-        assertEquals("gherkin-darwin-386", magicFile.getFileName());
+        assertEquals("gherkin-darwin-amd64", magicFile.getFileName());
+    }
+
+    @Test
+    public void generates_file_name_for_windows() {
+        MagicFile magicFile = new MagicFile("gherkin-{{.OS}}-{{.Arch}}{{.Ext}}", new HashMap<Object, Object>() {{
+            put("os.name", "Windows 10");
+            put("os.arch", "x86_32");
+        }});
+        assertEquals("gherkin-windows-386.exe", magicFile.getFileName());
     }
 
     @Test
@@ -69,7 +78,7 @@ public class MagicFileTest {
 
     @Test
     public void throws_exception_with_explanation_when_file_not_found() {
-        expected.expectMessage("Failed to download https://s3.eu-west-2.amazonaws.com/io.cucumber/gherkin-go/master/notfound-darwin-386");
+        expected.expectMessage("Failed to download https://s3.eu-west-2.amazonaws.com/io.cucumber/gherkin-go/master/notfound-darwin-amd64");
         MagicFile magicFile = new MagicFile("notfound-{{.OS}}-{{.Arch}}", new HashMap<Object, Object>() {{
             put("os.name", "Mac OS X");
             put("os.arch", "x86_64");
