f5db7f906fa490258e5dfd420d68a8f33c13dd91
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-07-17 08:25:05 +0100

    Don't cross compile go executables on Docker/Alpine

--- .templates/go/default.mk
+++ .templates/go/default.mk
@@ -1,4 +1,5 @@
 SHELL := /usr/bin/env bash
+ALPINE := $(shell which apk 2> /dev/null)
 GOPATH := $(shell go env GOPATH)
 PATH := $(PATH):$(GOPATH)/bin
 GO_SOURCE_FILES = $(shell find . -name "*.go")
@@ -14,7 +15,12 @@ ifeq ($(IN_GOPATH), 0)
 endif
 
 ifneq (,$(wildcard ./cli))
+ifndef ALPINE
+# Cross-compile executables if there is a CLI. Disabled on Alpine Linux builds
+# (monorepo build in Docker) where cross  compilation fails for certain platforms.
+# Subrepo builds don't run in Docker/Alpine, so cross compile will happen there.
 default: .dist
+endif
 endif
 
 default: .tested
