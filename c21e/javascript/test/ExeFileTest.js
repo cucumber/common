a7d741c3944ffdc935bd383cffb5d051aade92b6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-08-26 22:54:00 +0100

    c21e: javascript impl.

--- c21e/javascript/test/ExeFileTest.js
+++ c21e/javascript/test/ExeFileTest.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 const assert = require('assert')
-const ExeFile = require('../../src/exe/ExeFile')
+const ExeFile = require('../src/ExeFile')
 
 describe('ExeFile', () => {
   it('detects macos', () => {
@@ -10,5 +10,14 @@ describe('ExeFile', () => {
     })
 
     assert.strictEqual(exeFile.fileName, 'gherkin-darwin-amd64')
+  })
+
+  it('detects windows', () => {
+    const exeFile = new ExeFile('gherkin-{{.OS}}-{{.Arch}}{{.Ext}}', {
+      os: 'win32',
+      arch: 'x32',
+    })
+
+    assert.strictEqual(exeFile.fileName, 'gherkin-windows-386.exe')
   })
 })
