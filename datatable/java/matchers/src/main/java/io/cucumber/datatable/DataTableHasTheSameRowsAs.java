aa7743332db14fb667a349fba8707bc0e467fdfe
John Patrick <nhoj.patrick@gmail.com>
2018-09-08 18:12:32 +0100

    JPMS TODO

--- datatable/java/matchers/src/main/java/io/cucumber/datatable/DataTableHasTheSameRowsAs.java
+++ datatable/java/matchers/src/main/java/io/cucumber/datatable/DataTableHasTheSameRowsAs.java
@@ -5,6 +5,7 @@ import org.hamcrest.Factory;
 /**
  * @deprecated use {@link io.cucumber.datatable.matchers.DataTableHasTheSameRowsAs}
  */
+// TODO class needs to be deleted before cucumber fully supports Java 9 Modules as multiple modules can't export the same package
 @Deprecated
 public class DataTableHasTheSameRowsAs extends io.cucumber.datatable.matchers.DataTableHasTheSameRowsAs {
     private DataTableHasTheSameRowsAs(DataTable expectedValue, boolean unordered) {
