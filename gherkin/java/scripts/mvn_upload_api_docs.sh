87a2f2d72e4bb84f05d5af8e5f5a48f853f68cb4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-09-17 21:55:59 +0100

    Tweak Java deploy scripts

--- gherkin/java/scripts/mvn_upload_api_docs.sh
+++ gherkin/java/scripts/mvn_upload_api_docs.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
-
 #
 # Script that generates API docs and puts them in the Web Site's source tree.
 #
-if  [ "$TRAVIS_PULL_REQUEST" == 'true' ]; then
-  exit 0
-fi
-
-
-# Fetch the current tag
-TAG=`git describe --exact-match --abbrev=0`
-
-if  [[ -z ${TAG} ]]; then
-  echo "API Docs are only uploaded on a tagged build"
-  exit 0
-fi
+GIT_TAG=$1
 
 # Clone the repo
 git clone --depth=1 https://github.com/cucumber/api.cucumber.io.git
-mkdir -p api.cucumber.io/cucumber-jvm/${TAG}
+mkdir -p api.cucumber.io/cucumber-jvm/${GIT_TAG}
 
 # Create the javadoc
 mvn --quiet javadoc:aggregate
-cp -R target/site/apidocs/ api.cucumber.io/cucumber-jvm/${TAG}/javadoc
+cp -R target/site/apidocs/ api.cucumber.io/cucumber-jvm/${GIT_TAG}/javadoc
 
 # Setup up credentials
 cd api.cucumber.io
@@ -30,7 +18,7 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 # Commit and push
-git add cucumber-jvm/${TAG}/javadoc
-git commit -m "Add javadoc for cucumber-jvm ${TAG}"
+git add cucumber-jvm/${GIT_TAG}/javadoc
+git commit -m "Add javadoc for cucumber-jvm ${GIT_TAG}"
 git push
 
