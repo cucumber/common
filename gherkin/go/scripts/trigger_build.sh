d47b71eb723b90761f667a5f6e11eb8222d6dddc
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-09-22 00:28:13 +0100

    Try automatic deployment using rebuilds

--- gherkin/go/scripts/trigger_build.sh
+++ gherkin/go/scripts/trigger_build.sh
@@ -5,23 +5,56 @@
 set -euf -o pipefail
 
 # foo/bar => foo%2Fbar
-repo=$(echo "$1" | sed 's|/|%2F|')
-tag=$2
-token=$3
+org=$1
+repo=$2
+tag=$3
+token=$4
+org_repo="${org}%2F${repo}"
+# body_fmt='{
+#   "request": {
+#     "tag":"%s",
+#     "message": "Trigger build of tag %s"
+#   }
+# }'
+# 
+# body=$(printf "${body_fmt}" "${tag}" "${tag}")
 
-body_fmt='{
-  "request": {
-    "branch":"%s",
-    "message": "Trigger build of tag %s"
-  }
-}'
+# curl -v -s -X POST \
+#    -H "Content-Type: application/json" \
+#    -H "Accept: application/json" \
+#    -H "Travis-API-Version: 3" \
+#    -H "Authorization: token ${token}" \
+#    -d "${body}" \
+#    https://api.travis-ci.org/repo/${repo}/requests
 
-body=$(printf "${body_fmt}" "${tag}" "${tag}")
 
-curl -v -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token ${token}" \
-   -d "${body}" \
-   https://api.travis-ci.org/repo/${repo}/requests
+build=$(curl \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token ${token}" \
+    "https://api.travis-ci.org/repo/${org_repo}/builds"
+)
+
+build_id=$(echo "${build}" | jq "[.builds[] | select(.tag.name == \"${tag}\")][0] | .id")
+build_state=$(echo "${build}" | jq --raw-output "[.builds[] | select(.tag.name == \"${tag}\")][0] | .state")
+
+echo "build_state=${build_state}"
+
+if [ "$build_state" = "started" ]; then
+    echo "Cancelling..."
+    curl -X POST \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -H "Travis-API-Version: 3" \
+        -H "Authorization: token ${token}" \
+        "https://api.travis-ci.org/build/${build_id}/cancel"
+fi
+
+echo "Restarting..."
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token ${token}" \
+    "https://api.travis-ci.org/build/${build_id}/restart"
