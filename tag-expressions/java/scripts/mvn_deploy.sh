b62a3fcb16a299c578929e66a86b185dc5190c11
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-09-24 21:37:16 +0100

    Build tweaks

--- tag-expressions/java/scripts/mvn_deploy.sh
+++ tag-expressions/java/scripts/mvn_deploy.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
-else
-  echo "Artifacts are only deployed on a build of the master branch"
-fi
+mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
