19c3b89668ddb99e47e8a6e111ad600ef27cca99
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-10-14 01:49:33 +0100

    Simplify release process

--- config/java/scripts/decrypt_signing_key.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euf -o pipefail
-openssl aes-256-cbc -K $encrypted_0f645401adb0_key -iv $encrypted_0f645401adb0_iv -in scripts/codesigning.asc.enc -out scripts/codesigning.asc -d
--- datatable/java/scripts/decrypt_signing_key.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euf -o pipefail
-openssl aes-256-cbc -K $encrypted_1cfff19a9ef3_key -iv $encrypted_1cfff19a9ef3_iv -in scripts/codesigning.asc.enc -out scripts/codesigning.asc -d
