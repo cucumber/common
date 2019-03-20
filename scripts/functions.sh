051339d62bd119479b33694806622fd27cad0941
Ciaran McNulty <mail@ciaranmcnulty.com>
2019-03-20 15:19:49 +0000

    Fall back to md5 if md5sum is not available

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -35,7 +35,13 @@ function echo_blue
 function docker_image() {
   dockerfile=$1
   image_name=$(basename ${dockerfile} | cut -d '.' -f 2)
-  tag=$(cat ${dockerfile} | md5sum | cut -d ' ' -f 1)
+
+  if hash md5sum 2>/dev/null; then
+    tag=$(cat ${dockerfile} | md5sum | cut -d ' ' -f 1)
+  else
+    tag=$(cat ${dockerfile} | md5 -r | cut -d ' ' -f 1)
+  fi
+
   echo "cucumber/${image_name}:${tag}"
 }
 
