7d5a610da5532620437ecac650dfe47222a0f386
Steve Tooke <tooky@cucumber.io>
2019-03-22 16:04:49 +0000

    Prevent failure on deploy for tagged builds
    Co-authored-by: Aslak Hellesøy <aslak.hellesoy@gmail.com>

--- cucumber-messages/java/scripts/mvn_deploy.sh
+++ cucumber-messages/java/scripts/mvn_deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -eufx -o pipefail
 gpg -q --fast-import scripts/codesigning.asc
-mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
+if [ -z $TRAVIS_TAG ]; then
+  mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
+else
+  echo "Not deploying on tag build because we will deploy on the master build."
+  echo "The version in the pom.xml indicates whether this is a release build."
+fi
