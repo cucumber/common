499cc95e66d2a2b1b6b756f73eee920495e3efba
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-07-04 12:17:31 +0100

    Don't persist /usr/lib/node_modules to workspace. Override npm link.

--- tag-expressions/javascript/default.mk
+++ tag-expressions/javascript/default.mk
@@ -18,7 +18,6 @@ default: .built .published-snapshot
 
 .built: .deps .codegen $(TYPESCRIPT_SOURCE_FILES)
 	npm run build
-	npm link
 
 .tested: .deps .codegen $(TYPESCRIPT_SOURCE_FILES)
 	TS_NODE_TRANSPILE_ONLY=1 npm run test
