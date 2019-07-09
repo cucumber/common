65ce3c871484c4346088a98d60710da8559f6c03
Vincent Pretre <vincent.pretre@gmail.com>
2019-07-09 16:27:56 +0200

    Run cucumber-messages-publisher on the correct docker image
    Remove duplication
    Always load the repository workspace for Java jobs

--- c21e/java/default.mk
+++ c21e/java/default.mk
@@ -12,6 +12,7 @@ default: .tested
 	touch $@
 
 publish:
+	gpg --batch -q --fast-import ../../codesigning.key
 	mvn deploy -Psign-source-javadoc --settings scripts/ci-settings.xml -DskipTests=true
 .PHONY: publish
 
