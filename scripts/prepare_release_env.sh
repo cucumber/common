19704b822be19924173a3a2112385655fb2b59ef
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-07-15 12:16:00 +0100

    GPG config

--- scripts/prepare_release_env.sh
+++ scripts/prepare_release_env.sh
@@ -5,3 +5,8 @@ git-crypt unlock ~/git-crypt.key
 shopt -s dotglob
 cp -R secrets/* ~
 source ~/.bash_profile
+
+export GPG_TTY=$(tty)
+gpg --batch -q --fast-import secrets/codesigning.key
+echo "test" | gpg  --passphrase "${GPG_SIGNING_KEY_PASSPHRASE}" --batch --symmetric > /dev/null
+git config user.signingkey E60E1F911B996560FFB135DAF4CABFB5B89B8BE6
