2f4ca67841aa7e51afd1dd499764851f958a0578
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-07-15 12:23:47 +0100

    Start gpg-agent

--- scripts/prepare_release_env.sh
+++ scripts/prepare_release_env.sh
@@ -7,6 +7,7 @@ cp -R secrets/* ~
 source ~/.bash_profile
 
 export GPG_TTY=$(tty)
+eval $(gpg-agent --daemon --sh)
 gpg --batch -q --fast-import secrets/codesigning.key
 echo "test" | gpg  --passphrase "${GPG_SIGNING_KEY_PASSPHRASE}" --batch --symmetric > /dev/null
 git config user.signingkey E60E1F911B996560FFB135DAF4CABFB5B89B8BE6
