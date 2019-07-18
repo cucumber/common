0c1e11f14072e20535eb8c396b399a5679190e96
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-07-18 16:18:24 +0100

    Make runs rspec

--- cucumber-protocol/spec/petri_net_spec.rb
+++ cucumber-protocol/spec/petri_net_spec.rb
@@ -1,25 +1,26 @@
 require 'petri_net'
 
 describe PetriNet do
-  before do
-    @pn = PetriNet.build do
-      transition(:convert, in: :negative, out: :affirmative)
-      transition(:dissent, in: :affirmative, out: :negative)
-      transition(:yay, in: :vote, out: :affirmative)
-      transition(:nay, in: :vote, out: :negative)
-      token(:vote, 1)
+  describe 'voting' do
+    before do
+      @pn = PetriNet.build do
+        transition(:convert, in: :negative, out: :affirmative)
+        transition(:dissent, in: :affirmative, out: :negative)
+        transition(:yay, in: :vote, out: :affirmative)
+        transition(:nay, in: :vote, out: :negative)
+        token(:vote, 1)
+      end
+    end
+    
+    it "allows a transition" do
+      @pn.fire(:yay)
+    end
+
+    it "does not allow a transition" do
+      @pn.fire(:yay)
+      expect do
+        @pn.fire(:yay)
+      end.to raise_error('Cannot fire yay')
     end
   end
-  
-  it "allows a transition" do
-    @pn.fire(:yay)
-  end
-
-  it "does not allow a transition" do
-    @pn.fire(:yay)
-    expect do
-      @pn.fire(:yay)
-    end.to raise_error('Cannot fire yay')
-  end
-
 end
