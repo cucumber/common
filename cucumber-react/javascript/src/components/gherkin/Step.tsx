c5f6bab9e8ef5e720c1705529a448d78397d7a92
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-08-21 12:19:08 +0100

    Fix build issues

--- cucumber-react/javascript/src/components/gherkin/Step.tsx
+++ cucumber-react/javascript/src/components/gherkin/Step.tsx
@@ -12,23 +12,26 @@ import Color from 'color'
 const StepLi = styled.li`
   background-color: ${(props: IStepLiProps) => {
     return {
-      [Status.AMBIGUOUS]: 'gray',
-      [Status.FAILED]: Color('red')
-        .lighten(0.9)
-        .hex(),
+      // Keep the same order as in messages.proto - for readability's sake
       [Status.PASSED]: Color('lime')
         .lighten(0.9)
         .hex(),
-      [Status.PENDING]: Color('yellow')
+      [Status.SKIPPED]: Color('cyan')
         .lighten(0.9)
         .hex(),
-      [Status.SKIPPED]: Color('cyan')
+      [Status.PENDING]: Color('yellow')
         .lighten(0.9)
         .hex(),
       [Status.UNDEFINED]: Color('orange')
         .lighten(0.9)
         .hex(),
       [Status.AMBIGUOUS]: Color('rebeccapurple')
+        .lighten(0.9)
+        .hex(),
+      [Status.FAILED]: Color('red')
+        .lighten(0.9)
+        .hex(),
+      [Status.UNKNOWN]: Color('gray')
         .lighten(0.9)
         .hex(),
     }[props.status]
