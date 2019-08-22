ca268d384a6d7e15f348c6ac3613d1eb067081c2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-08-22 13:11:31 +0100

    More build tweaks

--- c21e/javascript/default.mk
+++ c21e/javascript/default.mk
@@ -52,5 +52,5 @@ clean: clean-javascript
 .PHONY: clean
 
 clean-javascript:
-	rm -rf .codegen .built .tested .linted package-lock.json node_modules coverage dist
+	rm -rf .deps .codegen .built .tested .linted package-lock.json node_modules coverage dist
 .PHONY: clean-javascript
