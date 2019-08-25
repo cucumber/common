a8ae0046c3c6f4cff1c547f2a502825ff1cd4e32
Jonas Amundsen <jonasba@gmail.com>
2019-08-25 20:27:40 +0200

    Initial work on restoring the native parser
    This is essentially a checkout of the parser as of #353. I've scoped the
    content to src/legacy for now.
    
    [1] https://github.com/cucumber/cucumber/pull/353

---
+++ gherkin/javascript/test/legacy/stream/event_stream_test.js
@@ -0,0 +1,37 @@
+const assert = require('assert')
+const fs = require('fs')
+const EventStream = require('../../lib/gherkin/stream/event_stream')
+
+describe('EventStream', () => {
+  it("transforms input to stream", (callback) => {
+    const events = []
+    const eventStream = new EventStream('test.feature', {
+      'source': true,
+      'gherkin-document': true,
+      'pickle': true
+    })
+    eventStream.on('data', data => events.push(data))
+    eventStream.on('end', () => {
+      assert.equal(events.length, 3)
+      assert.deepEqual(events.map(e => e.uri), ['test.feature', 'test.feature', 'test.feature'])
+      callback()
+    })
+    fs.createReadStream(__dirname + '/test.feature', { encoding: 'utf-8' }).pipe(eventStream)
+  })
+
+  it("accepts a language parameter", (callback) => {
+    const events = []
+    const eventStream = new EventStream('test_fr.feature', {
+      'source': true,
+      'gherkin-document': true,
+      'pickle': true
+    }, 'fr')
+    eventStream.on('data', data => events.push(data))
+    eventStream.on('end', () => {
+      assert.equal(events.length, 3)
+      assert.deepEqual(events.map(e => e.uri), ['test_fr.feature', 'test_fr.feature', 'test_fr.feature'])
+      callback()
+    })
+    fs.createReadStream(__dirname + '/test_fr.feature', { encoding: 'utf-8' }).pipe(eventStream)
+  })
+})
