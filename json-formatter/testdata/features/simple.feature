fde434cbcc7ec9feefc57ef9ab7534093dd2b4b8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-09-04 16:15:56 +0100

    Add build scripts for generating golden master

---
+++ json-formatter/testdata/features/simple.feature
@@ -0,0 +1,5 @@
+Feature: Simple
+  Scenario: number 1
+    Given a passing step
+    When a failing step
+    Then a skipped step
---
+++ json-formatter/testdata/features/step_definitions/steps.rb
@@ -0,0 +1,9 @@
+Given("a passing step") do
+end
+
+When("a failing step") do
+  raise "Some error"
+end
+
+Then("a skipped step") do
+end
