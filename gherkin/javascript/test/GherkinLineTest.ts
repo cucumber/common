2f12913f15d2db3b00d4bbb5b773ff3927945c9b
Vincent Pretre <vincent.pretre@gmail.com>
2019-10-23 17:02:34 +0200

    [gherkin / javascript] Split tests to make them more explicit.
    Also wrap tests about getTableCells in its own 'describe'

--- gherkin/javascript/test/GherkinLineTest.ts
+++ gherkin/javascript/test/GherkinLineTest.ts
@@ -1,20 +1,28 @@
 import * as assert from 'assert'
 import GherkinLine from '../src/GherkinLine'
 
-describe('GherkinLine', function() {
-  function getCellsText(line: string) {
-    const gl = new GherkinLine(line, 1)
+describe('GherkinLine', () => {
+  describe('#getTableCells', () => {
+    function getCellsText(line: string) {
+      const gl = new GherkinLine(line, 1)
 
-    return gl.getTableCells().map(span => span.text)
-  }
+      return gl.getTableCells().map(span => span.text)
+    }
 
-  it('getTableCells correctly trims cells content', function() {
-    assert.deepEqual(getCellsText("|spaces after   |"), ['spaces after'])
-    assert.deepEqual(getCellsText("|   \t spaces before|"), ['spaces before'])
-    assert.deepEqual(getCellsText("|   \t spaces everywhere   \t|"), ['spaces everywhere'])
-  })
+    it('trims white spaces before cell content', () => {
+      assert.deepEqual(getCellsText("|   \t spaces before|"), ['spaces before'])
+    })
 
-  it('getTableCells does not drop white spaces inside a cell', function() {
-    assert.deepEqual(getCellsText("| foo()\n  bar\nbaz |"), ['foo()\n  bar\nbaz'])
-  })
+    it('trims white spaces after cell content', () => {
+      assert.deepEqual(getCellsText("|spaces after   |"), ['spaces after'])
+    })
+
+    it('trims white spaces around cell content', () => {
+      assert.deepEqual(getCellsText("|   \t spaces everywhere   \t|"), ['spaces everywhere'])
+    })
+
+    it('does not delete white spaces inside a cell', () => {
+      assert.deepEqual(getCellsText("| foo()\n  bar\nbaz |"), ['foo()\n  bar\nbaz'])
+    })
+  });
 })
