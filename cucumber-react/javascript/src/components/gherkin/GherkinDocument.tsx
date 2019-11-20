ddd3214b8fcf3e75bd30a654d09b5c53dfdf164c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-11-20 14:12:26 +0000

    Return null when there is no feature

--- cucumber-react/javascript/src/components/gherkin/GherkinDocument.tsx
+++ cucumber-react/javascript/src/components/gherkin/GherkinDocument.tsx
@@ -10,7 +10,7 @@ const GherkinDocument: React.FunctionComponent<IProps> = ({
                                                             gherkinDocument,
                                                           }) => {
   return (
-    gherkinDocument.feature && <Feature feature={gherkinDocument.feature}/>
+    gherkinDocument.feature ? <Feature feature={gherkinDocument.feature}/> : null
   )
 }
 
