dfcad40cb3fb086f0c5ba58787fd073fabe927b2
M.P. Korstanje <rien.korstanje@gmail.com>
2019-12-04 20:54:05 +0100

    build: Check if all files are on the module path

--- gherkin/java/scripts/check-jar.sh
+++ gherkin/java/scripts/check-jar.sh
@@ -7,8 +7,10 @@ set -uf -o pipefail
 
 check_jar() {
   jar="$1"
+  module_name=$(xmlstarlet sel -N pom="http://maven.apache.org/POM/4.0.0"  -t -m "//pom:project.Automatic-Module-Name" -v . pom.xml)
+  module_path=$(echo $module_name | sed "s/\./\\\\\//g")
   echo "Checking contents of ${jar}"
-  unshaded_classes=$(unzip -l ${jar} | grep -e "\.class" | rev | cut -d' ' -f1 | rev | grep -v "^io\/cucumber\/")
+  unshaded_classes=$(unzip -l ${jar} | grep -e "\.class" | rev | cut -d' ' -f1 | rev | grep -v "^$module_path")
   if [[ "${unshaded_classes}" != "" ]]; then
     echo "Some classes in ${jar} are not in the io.cucumber package. Rename the classes or change the maven-shade-plugin configuration."
     echo
