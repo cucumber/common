71d2dc2a7f8a06363600632aeada7c06e59e2bd0
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-01-14 13:29:43 +0000

    improve signature

--- react/javascript/src/components/gherkin/isNumber.ts
+++ react/javascript/src/components/gherkin/isNumber.ts
@@ -1,6 +1,6 @@
 // Lifted from cucumber-expressions/javascript/src/ParameterTypeRegistry#FLOAT_REGEXP
 const numberPattern = /(?=.*\d.*)[-+]?\d*(?:\.(?=\d.*))?\d*(?:\d+[E][+-]?\d+)?/
 
-export default function isNumber(s: string) {
-  return s.match(numberPattern)
+export default function isNumber(s: string): boolean {
+  return !!s.match(numberPattern)
 }
