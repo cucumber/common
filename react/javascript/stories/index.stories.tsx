4fb8e366ebac972f4cbe38676377676dfe010499
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-01-16 16:26:20 +0000

    Add the CCK output to StoryBook

--- react/javascript/stories/index.stories.tsx
+++ react/javascript/stories/index.stories.tsx
@@ -4,16 +4,54 @@ import { messages } from '@cucumber/messages'
 import GherkinDocumentList from '../src/components/app/GherkinDocumentList'
 import '../src/styles/react-accessible-accordion.css'
 import '../src/styles/styles.css'
-// @ts-ignore
-import ndjson from '../testdata/all.ndjson'
 import Wrapper from '../src/components/app/Wrapper'
 
-const envelopes: messages.IEnvelope[] = ndjson.trim().split('\n')
-  .map((json: string) => messages.Envelope.fromObject(JSON.parse(json)))
+// @ts-ignore
+import documentList from '../testdata/all.ndjson'
+// @ts-ignore
+import attachments from '../../../compatibility-kit/javascript/features/attachments/attachments.ndjson'
+// @ts-ignore
+import dataTables from '../../../compatibility-kit/javascript/features/data-tables/data-tables.ndjson'
+// @ts-ignore
+import hooks from '../../../compatibility-kit/javascript/features/hooks/hooks.ndjson'
+// @ts-ignore
+import parameterTypes from '../../../compatibility-kit/javascript/features/parameter-types/parameter-types.ndjson'
+// @ts-ignore
+import stackTraces from '../../../compatibility-kit/javascript/features/stack-traces/stack-traces.ndjson'
+
+function envelopes(ndjson: string): messages.IEnvelope[] {
+  return ndjson.trim().split('\n')
+    .map((json: string) => messages.Envelope.fromObject(JSON.parse(json)))
+}
 
 storiesOf('Features', module)
   .add('Document list', () => {
-    return <Wrapper envelopes={envelopes}>
+    return <Wrapper envelopes={envelopes(documentList)}>
+      <GherkinDocumentList/>
+    </Wrapper>
+  })
+  .add('Attachments', () => {
+    return <Wrapper envelopes={envelopes(attachments)}>
+      <GherkinDocumentList/>
+    </Wrapper>
+  })
+  .add('Data Tables', () => {
+    return <Wrapper envelopes={envelopes(dataTables)}>
+      <GherkinDocumentList/>
+    </Wrapper>
+  })
+  .add('Hooks', () => {
+    return <Wrapper envelopes={envelopes(hooks)}>
+      <GherkinDocumentList/>
+    </Wrapper>
+  })
+  .add('Parameter Types', () => {
+    return <Wrapper envelopes={envelopes(parameterTypes)}>
+      <GherkinDocumentList/>
+    </Wrapper>
+  })
+  .add('Stack Traces', () => {
+    return <Wrapper envelopes={envelopes(stackTraces)}>
       <GherkinDocumentList/>
     </Wrapper>
   })
