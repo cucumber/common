279d1260c93c6428ce8a41597d8724596b70b5be
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-01-31 17:11:27 +0000

    Relax float comparison

--- messages/ruby/spec/cucumber/messages/time_conversion_spec.rb
+++ messages/ruby/spec/cucumber/messages/time_conversion_spec.rb
@@ -26,7 +26,7 @@ module Cucumber
         duration = seconds_to_duration(duration_in_seconds)
         duration_in_seconds_again = duration_to_seconds(duration)
 
-        expect(duration_in_seconds_again).to eq(duration_in_seconds)
+        expect(duration_in_seconds_again).to be_within(0.000000001).of(duration_in_seconds)
       end
     end
   end
