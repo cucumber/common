e1481238b405b5b8c9f63293e082c55b8a193bea
M.P. Korstanje <rien.korstanje@gmail.com>
2020-02-07 23:48:06 +0100

    Fix ruby proper

--- gherkin/ruby/lib/gherkin/gherkin_line.rb
+++ gherkin/ruby/lib/gherkin/gherkin_line.rb
@@ -37,8 +37,8 @@ module Gherkin
       cells = []
 
       self.split_table_cells(@trimmed_line_text) do |item, column|
-        txt_trimmed_left = item.sub(/^[ \t\v\f\r\u0085\u00A0]*/, '')
-        txt_trimmed = txt_trimmed_left.sub(/[ \t\v\f\r\u0085\u00A0]*$/, '')
+        txt_trimmed_left = item.sub(/\A[ \t\v\f\r\u0085\u00A0]*/, '')
+        txt_trimmed = txt_trimmed_left.sub(/[ \t\v\f\r\u0085\u00A0]*\z/, '')
         cell_indent = item.length - txt_trimmed_left.length
         span = Span.new(@indent + column + cell_indent, txt_trimmed)
         cells.push(span)
