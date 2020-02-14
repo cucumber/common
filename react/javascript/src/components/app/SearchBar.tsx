9e8a59aa4edce864b22e8729d06e4ec5b0684c2a
Bastien Malatier <bastien.malatier@smartbear.com>
2020-02-14 11:30:06 +0100

    Add styles to search bar and Enter Key

--- react/javascript/src/components/app/SearchBar.tsx
+++ react/javascript/src/components/app/SearchBar.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface IProps {
   queryUpdated: (query: string) => any
@@ -7,10 +9,22 @@ interface IProps {
 const SearchBar: React.FunctionComponent<IProps> = ({ queryUpdated }) => {
   const [query, setQuery] = useState('')
 
+  const updateQueryOnEnter = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      queryUpdated(query)
+    }
+  }
+
   return (
     <div className="cucumber-search-bar">
-      <input type="text" onChange={event => setQuery(event.target.value)} />
-      <input type="button" onClick={() => queryUpdated(query)} value="go" />
+      <input
+        type="text"
+        onKeyPress={updateQueryOnEnter}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button type="submit" onClick={() => queryUpdated(query)} value="search">
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
     </div>
   )
 }
