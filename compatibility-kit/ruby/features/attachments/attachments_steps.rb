d35ecc8b7c02e2690b8d50abbb0b8d6a2e04a9d9
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-14 16:31:24 +0100

    Awful monkey patch to be able to attach a bute stream for Cucumber 3 + json reporter This should be fixed and backported for a new cucumber 3 release.

--- compatibility-kit/ruby/features/attachments/attachments_steps.rb
+++ compatibility-kit/ruby/features/attachments/attachments_steps.rb
@@ -10,6 +10,28 @@ When('the string {string} is attached as {string}') do |text, media_type|
 end
 
 When('an array with {int} bytes are attached as {string}') do |size, media_type|
+  # Awful monkey patch here.
+  module Cucumber
+    module Formatter
+      class Json
+        def embed(src, mime_type, _label)
+          is_file = File.file?(src) rescue false
+
+          if is_file
+            content = File.open(src, 'rb', &:read)
+            data = encode64(content)
+          elsif mime_type =~ /;base64$/
+            mime_type = mime_type[0..-8]
+            data = src
+          else
+            data = encode64(src)
+          end
+          test_step_embeddings << { mime_type: mime_type, data: data }
+        end
+      end
+    end
+  end
+
   data = (0..size-1).map {|i| [i].pack('C') }.join
   embed(data, media_type)
 end
