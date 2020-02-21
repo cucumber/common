ae4cd00c1758a10b27a8c6f676ff5a569b75c2f3
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-21 14:08:39 +0100

    Update step definitions in compatibility-kit/ruby to avoid deprecation warnings.

--- compatibility-kit/ruby/features/attachments/attachments_steps.rb
+++ compatibility-kit/ruby/features/attachments/attachments_steps.rb
@@ -5,8 +5,13 @@ Before do
   # no-op
 end
 
+def attach_or_embed(world, data, media_type)
+  # Backward compatibility as the steps are also used by cucumber-ruby 3 which does not support `attach`
+  world.respond_to?(:attach) ? attach(data, media_type) : embed(data, media_type)
+end
+
 When('the string {string} is attached as {string}') do |text, media_type|
-  embed(text, media_type)
+  attach_or_embed(self, text, media_type)
 end
 
 When('an array with {int} bytes are attached as {string}') do |size, media_type|
@@ -33,7 +38,7 @@ When('an array with {int} bytes are attached as {string}') do |size, media_type|
   end
 
   data = (0..size-1).map {|i| [i].pack('C') }.join
-  embed(data, media_type)
+  attach_or_embed(self, data, media_type)
 end
 
 When('a stream with {int} bytes are attached as {string}') do |size, media_type|
@@ -41,9 +46,9 @@ When('a stream with {int} bytes are attached as {string}') do |size, media_type|
   stream.puts (0..size).map(&:to_s).join('')
   stream.seek(0)
 
-  embed(stream, media_type)
+  attach_or_embed(self, stream, media_type)
 end
 
 When('a JPEG image is attached') do
-  embed(File.open("#{__dir__}/cucumber-growing-on-vine.jpg"), 'image/jpg')
+  attach_or_embed(self, File.open("#{__dir__}/cucumber-growing-on-vine.jpg"), 'image/jpg')
 end
