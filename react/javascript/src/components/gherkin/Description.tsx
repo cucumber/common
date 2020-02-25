42daf647efe4f55d1fc45c39609c3bcbe3cd6e62
codemrkay <kay@MacBook-Air-KAY.local>
2020-02-25 19:12:34 +0100

    Markdown in Description component

--- react/javascript/src/components/gherkin/Description.tsx
+++ react/javascript/src/components/gherkin/Description.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import marked from 'marked'
+import sanitizeHtml from 'sanitize-html'
 
 interface IProps {
   description: string
 }
 
+const sanitize = (toSanitize: string) => {
+  return sanitizeHtml(toSanitize)
+}
+
+const markdown = (toMarkdown: string) => {
+  const sanitized = sanitize(toMarkdown)
+  const convertedToMarkdown = marked(sanitized)
+  return { __html: convertedToMarkdown }
+}
+
 const Description: React.FunctionComponent<IProps> = ({ description }) => {
-  return <p>{description}</p>
+  return (
+    <p>
+      <div dangerouslySetInnerHTML={markdown(description)} />
+    </p>
+  )
 }
 
 export default Description
