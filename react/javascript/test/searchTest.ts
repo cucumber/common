7276c701d7c15a64c3b329bf3f10afdb595fb069
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-02-25 13:47:15 +0000

    Add elasticlunr to code

--- react/javascript/test/searchTest.ts
+++ react/javascript/test/searchTest.ts
@@ -1,10 +1,16 @@
 import assert from 'assert'
 import { messages } from '@cucumber/messages'
+import elasticlunr from 'elasticlunr'
 
 function search(
   gherkinDocuments: messages.IGherkinDocument[],
   query: string
 ): messages.IGherkinDocument[] {
+
+  const index = elasticlunr(function () {
+    this.addField('featureName')
+  })
+
   return gherkinDocuments.filter(
     gherkinDocument => gherkinDocument.feature.name.includes(query)
   )
@@ -35,6 +41,17 @@ describe('search', () => {
       }),
     })
     const searchResult = search([gherkinDocument], 'exists')
+
+    assert.deepStrictEqual(searchResult, [gherkinDocument])
+  })
+
+  it('finds results with substring of feature name with typo', () => {
+    const gherkinDocument = messages.GherkinDocument.create({
+      feature: messages.GherkinDocument.Feature.create({
+        name: 'this exists',
+      }),
+    })
+    const searchResult = search([gherkinDocument], 'exits')
 
     assert.deepStrictEqual(searchResult, [gherkinDocument])
   })
