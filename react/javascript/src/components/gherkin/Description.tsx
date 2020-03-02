5217f4044eaa599717241520bcc221e67e8484d7
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-03-02 22:19:06 +0000

    Sanitize the HTML, not the Markdown. Simplify code.

--- react/javascript/src/components/gherkin/Description.tsx
+++ react/javascript/src/components/gherkin/Description.tsx
@@ -6,22 +6,10 @@ interface IProps {
   description: string
 }
 
-const sanitize = (toSanitize: string) => {
-  return sanitizeHtml(toSanitize)
-}
-
-const markdown = (toMarkdown: string) => {
-  const sanitized = sanitize(toMarkdown)
-  const convertedToMarkdown = marked(sanitized)
-  return { __html: convertedToMarkdown }
-}
-
 const Description: React.FunctionComponent<IProps> = ({ description }) => {
-  return (
-    <p>
-      <div dangerouslySetInnerHTML={markdown(description)} />
-    </p>
-  )
+  const html = marked(description)
+  const sanitizedHtml = sanitizeHtml(html)
+  return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
 }
 
 export default Description
