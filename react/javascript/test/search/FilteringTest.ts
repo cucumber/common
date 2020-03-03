289a0608647c26598c42fb580b58a5361f12c6ce
Vincent Pretre <vincent.pretre@gmail.com>
2020-03-03 16:08:40 +0100

    Add example of filtering

--- react/javascript/test/search/FilteringTest.ts
+++ react/javascript/test/search/FilteringTest.ts
@@ -1,41 +1,47 @@
 import assert from 'assert'
-import { messages } from '@cucumber/messages'
+import { messages, IdGenerator } from '@cucumber/messages'
 import Search from '../../src/search/Search'
 import { makeFeature, makeScenario, makeStep } from './utils'
+import Parser from '@cucumber/gherkin/dist/src/Parser'
+import AstBuilder from '@cucumber/gherkin/dist/src/AstBuilder'
+import pretty from '../../src/pretty-formatter/pretty'
 
 describe('Search', () => {
   let search: Search
   let gherkinDocuments: messages.IGherkinDocument[]
 
   beforeEach(() => {
-    gherkinDocuments = [
-      messages.GherkinDocument.create({
-        uri: 'some/feature.file',
-        feature: makeFeature('Continents', 'The world is divided in continents', [
-          makeScenario('Europe', 'The old continent', [
-            makeStep('Given', 'France'),
-            makeStep('When', 'Spain'),
-            makeStep('Then', 'The Netherlands'),
-          ]),
-          makeScenario('America', 'The new continent', [
-            makeStep('When', 'Mexico'),
-            makeStep('Then', 'Brazil'),
-          ]),
-        ]),
-      }),
-    ]
+    const source = `Feature: Continents
+
+  Scenario: Europe
+    Given France
+    When Spain
+    Then The Netherlands
+
+  Scenario: America
+    Given Mexico
+    Then Brazil
+  `
+
+    const newId = IdGenerator.uuid()
+    const parser = new Parser(new AstBuilder(newId))
+    const gherkinDocument = parser.parse(source)
 
     search = new Search()
-    for (const gherkinDocument of gherkinDocuments) {
-      search.add(gherkinDocument)
-    }
+    search.add(gherkinDocument)
   })
 
   context('Hit found in step', () => {
     it('displays just one scenario', () => {
       const searchResults = search.search('Spain')
 
-      assert.deepStrictEqual(searchResults, [])
+      assert.deepStrictEqual(pretty(searchResults[0]), `Feature: Continents
+
+  Scenario: Europe
+    Given France
+    When Spain
+    Then The Netherlands
+`)
     })
   })
 })
