2059f84d4044081c453b2bd8e9e33ef8247fbb6a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-03-03 14:43:58 +0000

    Basic pretty

--- react/javascript/test/pretty-formatter/PrettyFormatterTest.ts
+++ react/javascript/test/pretty-formatter/PrettyFormatterTest.ts
@@ -8,8 +8,30 @@ function pretty(gherkinDocument: messages.IGherkinDocument): string {
   let s = feature.keyword + ': ' + feature.name + '\n'
 
   for (const child of feature.children) {
-    if (child.scenario) {
+    if (child.background) {
+      s += `\n  ${child.background.keyword}: ${child.background.name}\n`
+
+      for (const step of child.background.steps) {
+        s += `    ${step.keyword}${step.text}\n`
+      }
+    } else if (child.scenario) {
       s += `\n  ${child.scenario.keyword}: ${child.scenario.name}\n`
+
+      for (const step of child.scenario.steps) {
+        s += `    ${step.keyword}${step.text}\n`
+      }
+    } else if (child.rule) {
+      s += `\n  ${child.rule.keyword}: ${child.rule.name}\n`
+
+      for (const ruleChild of child.rule.children) {
+        if (ruleChild.scenario) {
+          s += `\n    ${ruleChild.scenario.keyword}: ${ruleChild.scenario.name}\n`
+
+          for (const step of ruleChild.scenario.steps) {
+            s += `      ${step.keyword}${step.text}\n`
+          }
+        }
+      }
     }
   }
   return s
@@ -24,8 +46,34 @@ describe('PrettyFormatter', () => {
     assertPrettyIdentical(`Feature: hello
 
   Scenario: one
+    Given hello
 
   Scenario: two
+    Given world
+`)
+  })
+
+  it('renders a feature with two scenarios in a rule', () => {
+    assertPrettyIdentical(`Feature: hello
+
+  Rule: ok
+
+    Scenario: one
+      Given hello
+
+    Scenario: two
+      Given world
+`)
+  })
+
+  it('renders a feature with background and scenario', () => {
+    assertPrettyIdentical(`Feature: hello
+
+  Background: bbb
+    Given hello
+
+  Scenario: two
+    Given world
 `)
   })
 })
