ee1cf21b473cd009bb1fc22ef0417cd0f1312041
Vincent Pretre <vincent.pretre@gmail.com>
2020-03-03 16:02:15 +0100

    Extract pretty-formatter + refactor

--- react/javascript/test/pretty-formatter/PrettyFormatterTest.ts
+++ react/javascript/test/pretty-formatter/PrettyFormatterTest.ts
@@ -1,41 +1,8 @@
-import Parser from 'gherkin/dist/src/Parser'
-import AstBuilder from 'gherkin/dist/src/AstBuilder'
-import { IdGenerator, messages } from '@cucumber/messages'
+import Parser from '@cucumber/gherkin/dist/src/Parser'
+import AstBuilder from '@cucumber/gherkin/dist/src/AstBuilder'
+import { IdGenerator } from '@cucumber/messages'
 import * as assert from 'assert'
-
-function pretty(gherkinDocument: messages.IGherkinDocument): string {
-  const feature = gherkinDocument.feature
-  let s = feature.keyword + ': ' + feature.name + '\n'
-
-  for (const child of feature.children) {
-    if (child.background) {
-      s += `\n  ${child.background.keyword}: ${child.background.name}\n`
-
-      for (const step of child.background.steps) {
-        s += `    ${step.keyword}${step.text}\n`
-      }
-    } else if (child.scenario) {
-      s += `\n  ${child.scenario.keyword}: ${child.scenario.name}\n`
-
-      for (const step of child.scenario.steps) {
-        s += `    ${step.keyword}${step.text}\n`
-      }
-    } else if (child.rule) {
-      s += `\n  ${child.rule.keyword}: ${child.rule.name}\n`
-
-      for (const ruleChild of child.rule.children) {
-        if (ruleChild.scenario) {
-          s += `\n    ${ruleChild.scenario.keyword}: ${ruleChild.scenario.name}\n`
-
-          for (const step of ruleChild.scenario.steps) {
-            s += `      ${step.keyword}${step.text}\n`
-          }
-        }
-      }
-    }
-  }
-  return s
-}
+import pretty from '../../src/pretty-formatter/pretty'
 
 describe('PrettyFormatter', () => {
   it('renders a feature with no scenarios', () => {
