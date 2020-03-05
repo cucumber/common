ecb0d24b70305ea83ceb044ef3b538c76a3be3d1
David Goss <dvdgoss@gmail.com>
2020-03-05 22:41:34 +0000

    gherkin/javascript: make Dialect available on main export from gherkin package

--- gherkin/javascript/src/index.ts
+++ gherkin/javascript/src/index.ts
@@ -1,6 +1,7 @@
 import Gherkin from './Gherkin'
 import IGherkinOptions from './IGherkinOptions'
 import Query from './Query'
+import Dialect from './Dialect'
 
 export default Gherkin
-export { IGherkinOptions, Query }
+export { IGherkinOptions, Query, Dialect }
