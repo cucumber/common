cf54ea6e018264497a91b728ef2669f7a2118ddb
Bastien Malatier <bastien.malatier@smartbear.com>
2020-03-09 14:36:01 +0100

    WIP Ast Walker

---
+++ react/javascript/test/search/AstWalkerTest.ts
@@ -0,0 +1,64 @@
+import assert from 'assert'
+import { messages, IdGenerator } from '@cucumber/messages'
+import Parser from '@cucumber/gherkin/dist/src/Parser'
+import AstBuilder from '@cucumber/gherkin/dist/src/AstBuilder'
+
+class AstWalker {
+  private stepTextList:string[] = []
+
+  constructor(
+    private readonly gherkinDocument: messages.IGherkinDocument
+  ) {}
+
+  walkFeature(feature: messages.GherkinDocument.IFeature) {
+    for (const child of feature.children) {
+      if (child.background) {
+        this.walkBackground(child.background)
+      }
+    }
+  }
+
+  walkBackground(background: messages.GherkinDocument.Feature.IBackground) {
+    for(const step of background.steps) {
+      this.walkStep(step)
+    }
+  }
+
+  walkStep(step: messages.GherkinDocument.Feature.IStep) {
+    this.stepTextList.push(step.text)
+  }
+
+  stepsText(): string[] {
+    const feature = this.gherkinDocument.feature
+    this.walkFeature(feature)
+
+    return this.stepTextList
+  }
+}
+
+
+describe('AstWalker', () => {
+  let gherkinDocument: messages.IGherkinDocument
+  const source = `Feature: Solar System
+
+  Background: Universe
+  Given the universe exists
+`
+
+  beforeEach(() => {
+    const newId = IdGenerator.uuid()
+    const parser = new Parser(new AstBuilder(newId))
+    gherkinDocument = parser.parse(source)
+  })
+
+  context('.walk', () => {
+    it('reads steps inside a background', () => {
+      const astWalker = new AstWalker(gherkinDocument)
+
+      assert.deepStrictEqual(
+        astWalker.stepsText(),
+        ['the universe exists']
+      )
+    })
+  })
+})
