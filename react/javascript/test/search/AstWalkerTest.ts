d8e63100b0349b6bead1e27033e213223ff62948
Bastien Malatier <bastien.malatier@smartbear.com>
2020-03-09 15:15:02 +0100

    WIP Ast Walker Test / reads steps inside a scenario

--- react/javascript/test/search/AstWalkerTest.ts
+++ react/javascript/test/search/AstWalkerTest.ts
@@ -15,11 +15,20 @@ class AstWalker {
       if (child.background) {
         this.walkBackground(child.background)
       }
+      if (child.scenario) {
+        this.walkScenario(child.scenario)
+      }
     }
   }
 
   walkBackground(background: messages.GherkinDocument.Feature.IBackground) {
     for(const step of background.steps) {
+      this.walkStep(step)
+    }
+  }
+
+  walkScenario(scenario: messages.GherkinDocument.Feature.IScenario) {
+    for(const step of scenario.steps) {
       this.walkStep(step)
     }
   }
@@ -42,7 +51,10 @@ describe('AstWalker', () => {
   const source = `Feature: Solar System
 
   Background: Universe
-  Given the universe exists
+    Given the universe exists
+
+  Scenario: Saturn
+    Given is the sixth planet from the Sun
 `
 
   beforeEach(() => {
@@ -55,9 +67,16 @@ describe('AstWalker', () => {
     it('reads steps inside a background', () => {
       const astWalker = new AstWalker(gherkinDocument)
 
-      assert.deepStrictEqual(
-        astWalker.stepsText(),
-        ['the universe exists']
+      assert.ok(
+        astWalker.stepsText().includes('the universe exists')
+      )
+    })
+
+    it('reads steps inside a scenario', () => {
+      const astWalker = new AstWalker(gherkinDocument)
+
+      assert.ok(
+        astWalker.stepsText().includes('is the sixth planet from the Sun')
       )
     })
   })
