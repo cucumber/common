a498d3c723d71a26602a3d3a8f5cd4a6ad2944b2
Vincent Pretre <vincent.pretre@gmail.com>
2020-03-12 16:53:46 +0100

    Do not diplay result for hook steps

--- progress-formatter/ruby/lib/cucumber/progress_formatter.rb
+++ progress-formatter/ruby/lib/cucumber/progress_formatter.rb
@@ -5,11 +5,26 @@ module Cucumber
     class Formatter
       def initialize(out)
         @out = out
+        @hook_step_ids = []
       end
 
       def process_message(envelope)
+        if envelope.test_run_finished
+          @out.puts('')
+          return
+        end
+
+        if envelope.test_case
+          envelope.test_case.test_steps.each do |step|
+            @hook_step_ids << step.id unless step.hook_id.empty?
+          end
+        end
+
         return unless envelope.test_step_finished
+        return if @hook_step_ids.include?(envelope.test_step_finished.test_step_id)
+
         @out.print(CHARS[envelope.test_step_finished.test_step_result.status])
+        @out.flush
       end
 
       CHARS = {
--- progress-formatter/ruby/spec/progress_formatter_spec.rb
+++ progress-formatter/ruby/spec/progress_formatter_spec.rb
@@ -19,6 +19,36 @@ describe Cucumber::ProgressFormatter::Formatter do
     let(:passed) { make_test_step_finished(Cucumber::Messages::TestStepResult::Status::PASSED) }
     let(:failed) { make_test_step_finished(Cucumber::Messages::TestStepResult::Status::FAILED) }
 
+    let(:test_run_finished) {
+      Cucumber::Messages::Envelope.new({
+        test_run_finished: Cucumber::Messages::TestRunFinished.new()
+      })
+    }
+
+    let(:test_case) {
+      Cucumber::Messages::Envelope.new({
+        test_case: Cucumber::Messages::TestCase.new({
+          id: 'test-case-id',
+          test_steps: [
+            Cucumber::Messages::TestCase::TestStep.new({
+              id: 'hook-step-id',
+              hook_id: 'some-hook-id'
+            })
+          ]
+        })
+      })
+    }
+
+    let(:hook_step) {
+      Cucumber::Messages::Envelope.new({
+        test_step_finished: Cucumber::Messages::TestStepFinished.new(
+          test_step_id: test_case.test_case.test_steps.first.id,
+          test_step_result: Cucumber::Messages::TestStepResult.new(
+            status: Cucumber::Messages::TestStepResult::Status::FAILED
+          )
+        )
+      })
+    }
 
     it 'prints a . for passed step' do
       subject.process_message(passed)
@@ -35,12 +65,27 @@ describe Cucumber::ProgressFormatter::Formatter do
       expect(out.string).to be_empty
     end
 
+    it 'does not print status of Hook steps' do
+      subject.process_message(test_case)
+      subject.process_message(hook_step)
+      expect(out.string).to be_empty
+    end
+
     it 'does not add line break between messages' do
       subject.process_message(passed)
       subject.process_message(passed)
       subject.process_message(failed)
 
       expect(out.string).to eq('..F')
+    end
+
+    it 'adds a line return after TestRunFinished message is received' do
+      subject.process_message(passed)
+      subject.process_message(passed)
+      subject.process_message(failed)
+      subject.process_message(test_run_finished)
+
+      expect(out.string).to eq("..F\n")
     end
   end
 end
