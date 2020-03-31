64e9c2b1075d83fd0d373435547b7644f731f8a9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-03-31 09:49:13 +0100

    Export Parser

--- gherkin/javascript/src/index.ts
+++ gherkin/javascript/src/index.ts
@@ -1,8 +1,19 @@
 import Gherkin from './Gherkin'
 import IGherkinOptions from './IGherkinOptions'
 import Query from './Query'
-import compile from './pickles/compile'
 import Dialect from './Dialect'
+import Parser from './Parser'
+import AstBuilder from './AstBuilder'
+import TokenScanner from './TokenScanner'
+import compile from './pickles/compile'
 
 export default Gherkin
-export { IGherkinOptions, Query, Dialect, compile }
+export {
+  IGherkinOptions,
+  Query,
+  Dialect,
+  Parser,
+  AstBuilder,
+  TokenScanner,
+  compile,
+}
