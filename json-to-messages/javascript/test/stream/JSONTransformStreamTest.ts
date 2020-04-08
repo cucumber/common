3991ad2fe396e1e7e40c4f80f55481dcade0da4f
Vincent Pretre <vincent.pretre@gmail.com>
2020-04-08 09:34:41 +0200

    Ensure test compatibility with Node < 12

--- json-to-messages/javascript/test/stream/JSONTransformStreamTest.ts
+++ json-to-messages/javascript/test/stream/JSONTransformStreamTest.ts
@@ -1,4 +1,4 @@
-import { pipeline, Readable } from 'stream'
+import { pipeline, Stream } from 'stream'
 import { promisify } from 'util'
 import assert from 'assert'
 import JSONTransformStream from '../../src/stream/JSONTransformStream'
@@ -8,8 +8,13 @@ const asyncPipeline = promisify(pipeline)
 describe('JSONReportStream', () => {
   it('transforms a readable stream to a JSON report', async () => {
     const data = [{}, {}, {}]
-    const jsonChunks = JSON.stringify(data).split('')
-    const readableStream = Readable.from(jsonChunks)
+
+    const readableStream = new Stream.Readable({
+      read() {
+        this.push(JSON.stringify(data))
+        this.push(null)
+      },
+    })
     const sink = new SingleObjectWritableStream<Array<any>>()
     await asyncPipeline(readableStream, new JSONTransformStream(), sink)
     assert.deepStrictEqual(sink.object, data)
