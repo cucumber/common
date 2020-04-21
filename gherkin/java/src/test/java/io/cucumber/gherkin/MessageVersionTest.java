f5af430f4949d01de0b508ff1c23ccf1017b1151
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-04-21 11:15:01 +0100

    Explain why this test is here

--- gherkin/java/src/test/java/io/cucumber/gherkin/MessageVersionTest.java
+++ gherkin/java/src/test/java/io/cucumber/gherkin/MessageVersionTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
-// This tests that the message library
+// This tests that the message library exposes its version. This test is hard to do in the
+// library itself since it requires running against a packaged version (jar).
 public class MessageVersionTest {
     @Test
     public void message_library_has_version() {
