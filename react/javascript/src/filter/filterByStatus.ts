fc54a9eeb967140ed6ec044e64106ea00055c7c0
Vincent Pretre <vincent.pretre@gmail.com>
2020-06-19 09:55:09 +0200

    Simplify code - ensure ndjson regeneration on steps modification

--- react/javascript/src/filter/filterByStatus.ts
+++ react/javascript/src/filter/filterByStatus.ts
@@ -6,34 +6,12 @@ import {
   rejectAllFilters,
 } from '@cucumber/gherkin-utils'
 
-function getPickleResult(
-  pickle: messages.IPickle,
-  cucumberQuery: CucumberQuery
-): messages.TestStepFinished.TestStepResult.Status {
-  const pickleStepIds = pickle.steps.map((step) => step.id)
-  const pickleStepResults = cucumberQuery.getPickleStepTestStepResults(
-    pickleStepIds
-  )
-
-  const stepResults = pickleStepResults.slice()
-  const hookTestSteps = cucumberQuery
-    .getBeforeHookSteps(pickle.id)
-    .concat(cucumberQuery.getAfterHookSteps(pickle.id))
-  hookTestSteps.forEach((step) => {
-    const results = cucumberQuery.getTestStepResults(step.id)
-    results.forEach((result) => stepResults.push(result))
-  })
-
-  return cucumberQuery.getWorstTestStepResult(stepResults).status
-}
-
 export default function filterByStatus(
   gherkinDocument: messages.IGherkinDocument,
   gherkinQuery: GherkinQuery,
   cucumberQuery: CucumberQuery,
   statuses: messages.TestStepFinished.TestStepResult.Status[]
 ): messages.IGherkinDocument {
-  const pickles = gherkinQuery.getPickles()
   const filters = {
     acceptScenario: (scenario: messages.GherkinDocument.Feature.IScenario) => {
       const pickleIds = gherkinQuery.getPickleIds(
@@ -41,11 +19,14 @@ export default function filterByStatus(
         scenario.id
       )
 
-      return pickles
-        .filter((pickle) => pickleIds.includes(pickle.id))
-        .map((pickle) => {
-          return statuses.includes(getPickleResult(pickle, cucumberQuery))
-        })
+      return pickleIds
+        .map((pickleId) =>
+          statuses.includes(
+            cucumberQuery.getWorstTestStepResult(
+              cucumberQuery.getPickleTestStepResults([pickleId])
+            ).status
+          )
+        )
         .includes(true)
     },
   }
