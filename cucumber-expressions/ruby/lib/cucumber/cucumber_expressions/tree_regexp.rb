ff89c19cf03b57ecd482989b8e719e1cbc176cf7
M.P. Korstanje <rien.korstanje@gmail.com>
2020-06-19 18:32:15 +0200

    ruby: clean up

--- cucumber-expressions/ruby/lib/cucumber/cucumber_expressions/tree_regexp.rb
+++ cucumber-expressions/ruby/lib/cucumber/cucumber_expressions/tree_regexp.rb
@@ -8,41 +8,10 @@ module Cucumber
 
       def initialize(regexp)
         @regexp = regexp.is_a?(Regexp) ? regexp : Regexp.new(regexp)
-        @stack = [GroupBuilder.new]
-        group_start_stack = []
-        escaping = false
-        char_class = false
-        source = @regexp.source
-
-        source.each_char.with_index do |c, i|
-          if c == '[' && !escaping
-            char_class = true
-          elsif c == ']' && !escaping
-            char_class = false
-          elsif c == '(' && !escaping && !char_class
-            group_start_stack.push(i)
-            group_builder = GroupBuilder.new
-            non_capturing = is_non_capturing(source, i)
-            if non_capturing
-              group_builder.set_non_capturing!
-            end
-            @stack.push(group_builder)
-          elsif c == ')' && !escaping && !char_class
-            gb = @stack.pop
-            group_start = group_start_stack.pop + 1
-            if gb.capturing?
-              gb.source = source[group_start...i]
-              @stack.last.add(gb)
-            else
-              gb.move_children_to(@stack.last)
-            end
-          end
-          escaping = c == '\\' && !escaping
-        end
-        @group_builder = @stack.pop
+        @group_builder = create_group_builder(@regexp)
       end
 
-      def is_non_capturing(source, i)
+      private def is_non_capturing(source, i)
         # Regex is valid. Bounds check not required.
         i = i + 1
         next_char = source[i]
@@ -82,6 +51,39 @@ module Cucumber
         @group_builder.build(match, group_indices)
       end
 
+      private def create_group_builder(regexp)
+        source = regexp.source
+        stack = [GroupBuilder.new]
+        group_start_stack = []
+        escaping = false
+        char_class = false
+        source.each_char.with_index do |c, i|
+          if c == '[' && !escaping
+            char_class = true
+          elsif c == ']' && !escaping
+            char_class = false
+          elsif c == '(' && !escaping && !char_class
+            group_start_stack.push(i)
+            group_builder = GroupBuilder.new
+            non_capturing = is_non_capturing(source, i)
+            if non_capturing
+              group_builder.set_non_capturing!
+            end
+            stack.push(group_builder)
+          elsif c == ')' && !escaping && !char_class
+            gb = stack.pop
+            group_start = group_start_stack.pop + 1
+            if gb.capturing?
+              gb.source = source[group_start...i]
+              stack.last.add(gb)
+            else
+              gb.move_children_to(stack.last)
+            end
+          end
+          escaping = c == '\\' && !escaping
+        end
+        stack.pop
+      end
     end
   end
 end
