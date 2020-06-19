b97f164feb437820bf442eb6dcf27392c3d46406
M.P. Korstanje <rien.korstanje@gmail.com>
2020-06-19 18:27:16 +0200

    ruby: handle negative lookbehind, independent capture group and inline flags

--- cucumber-expressions/ruby/lib/cucumber/cucumber_expressions/tree_regexp.rb
+++ cucumber-expressions/ruby/lib/cucumber/cucumber_expressions/tree_regexp.rb
@@ -10,47 +10,69 @@ module Cucumber
         @regexp = regexp.is_a?(Regexp) ? regexp : Regexp.new(regexp)
         @stack = [GroupBuilder.new]
         group_start_stack = []
-        last = nil
         escaping = false
-        @non_capturing_maybe = false
-        @name_capturing_maybe = false
         char_class = false
+        source = @regexp.source
 
-        @regexp.source.each_char.with_index do |c, n|
+        source.each_char.with_index do |c, i|
           if c == '[' && !escaping
             char_class = true
           elsif c == ']' && !escaping
             char_class = false
           elsif c == '(' && !escaping && !char_class
-            @stack.push(GroupBuilder.new)
-            group_start_stack.push(n + 1)
-            @non_capturing_maybe = false
+            group_start_stack.push(i)
+            group_builder = GroupBuilder.new
+            non_capturing = is_non_capturing(source, i)
+            if non_capturing
+              group_builder.set_non_capturing!
+            end
+            @stack.push(group_builder)
           elsif c == ')' && !escaping && !char_class
             gb = @stack.pop
-            group_start = group_start_stack.pop
+            group_start = group_start_stack.pop + 1
             if gb.capturing?
-              gb.source = @regexp.source[group_start...n]
+              gb.source = source[group_start...i]
               @stack.last.add(gb)
             else
               gb.move_children_to(@stack.last)
             end
-            end_group
-          elsif c == '?' && last == '('
-            @non_capturing_maybe = true
-          elsif (c == '<') && @non_capturing_maybe
-            @name_capturing_maybe = true
-          elsif (c == ':' || c == '!' || c == '=') && last == '?' && @non_capturing_maybe
-            end_non_capturing_group
-          elsif (c == '=' || c == '!') && last == '<' && @name_capturing_maybe
-            end_non_capturing_group
-          elsif @name_capturing_maybe
-            raise CucumberExpressionError.new("Named capture groups are not supported. See https://github.com/cucumber/cucumber/issues/329")
           end
-
           escaping = c == '\\' && !escaping
-          last = c
         end
         @group_builder = @stack.pop
+      end
+
+      def is_non_capturing(source, i)
+        # Regex is valid. Bounds check not required.
+        i = i + 1
+        next_char = source[i]
+        if next_char != '?'
+          # (X)
+          return false
+        end
+
+        i = i + 1
+        next_char = source[i]
+        if next_char != '<'
+          # (?:X)
+          # (?idmsuxU-idmsuxU)
+          # (?idmsux-idmsux:X)
+          # (?=X)
+          # (?!X)
+          # (?>X)
+          return true
+        end
+
+        i = i + 1
+        next_char = source[i]
+        if next_char == '=' || next_char == '!'
+          # (?<=X)
+          # (?<!X)
+          return true
+        end
+
+        # (?<name>X)
+        raise CucumberExpressionError.new("Named capture groups are not supported. See https://github.com/cucumber/cucumber/issues/329")
       end
 
       def match(s)
@@ -60,17 +82,6 @@ module Cucumber
         @group_builder.build(match, group_indices)
       end
 
-      private
-
-      def end_non_capturing_group
-        @stack.last.set_non_capturing!
-        end_group
-      end
-
-      def end_group
-        @non_capturing_maybe = false
-        @name_capturing_maybe = false
-      end
     end
   end
 end
