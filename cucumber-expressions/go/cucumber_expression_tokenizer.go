998fa0df8814520108b24f1aad6e0dd67fe85d16
M.P. Korstanje <rien.korstanje@gmail.com>
2020-09-16 20:40:46 +0200

    empty string passes

--- cucumber-expressions/go/cucumber_expression_tokenizer.go
+++ cucumber-expressions/go/cucumber_expression_tokenizer.go
@@ -4,6 +4,11 @@ type tokenizer func(expression string, current int) (int, token)
 
 func tokenize(expression string) ([]token, error) {
 	tokens := make([]token, 0)
+	index := 0
+
+	tokens = append(tokens, token{"", startOfLine, 0, 0})
+
+	tokens = append(tokens, token{"", endOfLine, index, index})
 
 	return tokens, nil
 }
