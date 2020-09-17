5d3197f908d3c161b90a6386e39ec93f6db241b4
M.P. Korstanje <rien.korstanje@gmail.com>
2020-09-17 17:26:50 +0200

    Extract acceptance tests for CucumberExpressions

--- cucumber-expressions/java/src/test/java/io/cucumber/cucumberexpressions/CucumberExpressionTest.java
+++ cucumber-expressions/java/src/test/java/io/cucumber/cucumberexpressions/CucumberExpressionTest.java
@@ -1,230 +1,68 @@
 package io.cucumber.cucumberexpressions;
 
+import com.google.gson.Gson;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
+import static java.nio.file.Files.newDirectoryStream;
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CucumberExpressionTest {
+class CucumberExpressionTest {
     private final ParameterTypeRegistry parameterTypeRegistry = new ParameterTypeRegistry(Locale.ENGLISH);
 
-    @Test
-    public void documents_match_arguments() {
-        String expr = "I have {int} cuke(s)";
-        Expression expression = new CucumberExpression(expr, parameterTypeRegistry);
-        List<Argument<?>> args = expression.match("I have 7 cukes");
-        assertEquals(7, args.get(0).getValue());
+    private static List<Path> test() throws IOException {
+        List<Path> paths = new ArrayList<>();
+        newDirectoryStream(Paths.get("testdata", "expression")).forEach(paths::add);
+        paths.sort(Comparator.naturalOrder());
+        return paths;
     }
 
-    @Test
-    public void matches_word() {
-        assertEquals(singletonList("blind"), match("three {word} mice", "three blind mice"));
+    @ParameterizedTest
+    @MethodSource
+    void test(@ConvertWith(FileToExpectationConverter.class) Expectation expectation) {
+        if (expectation.getException() == null) {
+            CucumberExpression expression = new CucumberExpression(expectation.getExpression(), parameterTypeRegistry);
+            List<Argument<?>> match = expression.match(expectation.getText());
+            List<?> values = match == null ? null : match.stream()
+                    .map(Argument::getValue)
+                    .collect(Collectors.toList());
+            assertEquals(expectation.getExpected(), new Gson().toJson(values));
+        } else {
+            Executable executable = () -> {
+                String expr = expectation.getExpression();
+                CucumberExpression expression = new CucumberExpression(expr, parameterTypeRegistry);
+                expression.match(expectation.getText());
+            };
+            CucumberExpressionException exception = assertThrows(CucumberExpressionException.class, executable);
+            assertThat(exception.getMessage(), Matchers.is(expectation.getException()));
+        }
     }
 
-    @Test
-    public void matches_alternation() {
-        assertEquals(emptyList(), match("mice/rats and rats\\/mice", "rats and rats/mice"));
-    }
+    // Misc tests
 
     @Test
-    public void matches_optional_before_alternation_1() {
-        assertEquals(emptyList(), match("three (brown )mice/rats", "three brown mice"));
-    }
-
-    @Test
-    public void matches_optional_before_alternation_2() {
-        assertEquals(emptyList(), match("three (brown )mice/rats", "three rats"));
-    }
-
-    @Test
-    public void matches_optional_in_alternation_1() {
-        assertEquals(singletonList(3), match("{int} rat(s)/mouse/mice", "3 rats"));
-    }
-
-    @Test
-    public void matches_optional_in_alternation_2() {
-        assertEquals(singletonList(2), match("{int} rat(s)/mouse/mice", "2 mice"));
-    }
-
-    @Test
-    public void matches_optional_in_alternation_3() {
-        assertEquals(singletonList(1), match("{int} rat(s)/mouse/mice", "1 mouse"));
-    }
-
-    @Test
-    public void matches_optional_before_alternation_with_regex_characters_1() {
-        assertEquals(singletonList(2), match("I wait {int} second(s)./second(s)?", "I wait 2 seconds?"));
-    }
-
-    @Test
-    public void matches_optional_before_alternation_with_regex_characters_2() {
-        assertEquals(singletonList(1), match("I wait {int} second(s)./second(s)?", "I wait 1 second."));
-    }
-
-    @Test
-    public void matches_alternation_in_optional_as_text() {
-        assertEquals(emptyList(), match("three( brown/black) mice", "three brown/black mice"));
-    }
-
-    @Test
-    public void does_not_allow_empty_optional() {
-        Executable testMethod = () -> match("three () mice", "three brown mice");
-        CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat(thrownException.getMessage(), is(equalTo("" +
-                "This Cucumber Expression has a problem at column 7:\n" +
-                "\n" +
-                "three () mice\n" +
-                "      ^^\n" +
-                "An optional must contain some text.\n" +
-                "If you did not mean to use an optional you can use '\\(' to escape the the '('")));
-    }
-
-    @Test
-    public void does_not_allow_alternation_with_empty_alternative() {
-        Executable testMethod = () -> match("three brown//black mice", "three brown mice");
-        CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat(thrownException.getMessage(), is(equalTo("This Cucumber Expression has a problem at column 13:\n" +
-                "\n" +
-                "three brown//black mice\n" +
-                "            ^\n" +
-                "Alternative may not be empty." +
-                "\nIf you did not mean to use an alternative you can use '\\/' to escape the the '/'")));
-    }
-
-    @Test
-    public void does_not_allow_alternation_with_empty_alternative_by_adjacent_optional() {
-        Executable testMethod = () -> match("three (brown)/black mice", "three brown mice");
-        CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat(thrownException.getMessage(),
-                is(equalTo("" +
-                        "This Cucumber Expression has a problem at column 7:\n" +
-                        "\n" +
-                        "three (brown)/black mice\n" +
-                        "      ^-----^\n" +
-                        "An alternative may not exclusively contain optionals.\n" +
-                        "If you did not mean to use an optional you can use '\\(' to escape the the '('")));
-    }
-
-    @Test
-    public void does_not_allow_alternation_with_empty_alternative_by_adjacent_right_parameter() {
-
-        final Executable testMethod = () -> match("x/{int}", "3");
-
-        final CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat(thrownException.getMessage(), is(equalTo("" +
-                "This Cucumber Expression has a problem at column 3:\n" +
-                "\n" +
-                "x/{int}\n" +
-                "  ^\n" +
-                "Alternative may not be empty.\n" +
-                "If you did not mean to use an alternative you can use '\\/' to escape the the '/'")));
-    }
-
-    @Test
-    public void does_allow_parameter_adjacent_to_alternation() {
-        assertEquals(singletonList(3), match("{int}st/nd/rd/th", "3rd"));
-    }
-
-    @Test
-    public void does_not_allow_alternation_with_empty_alternative_by_adjacent_left_parameter() {
-
-        final Executable testMethod = () -> match("{int}/x", "3");
-
-        final CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat(thrownException.getMessage(), is(equalTo("" +
-                "This Cucumber Expression has a problem at column 6:\n" +
-                "\n" +
-                "{int}/x\n" +
-                "     ^\n" +
-                "Alternative may not be empty.\n" +
-                "If you did not mean to use an alternative you can use '\\/' to escape the the '/'")));
-    }
-
-    @Test
-    public void matches_double_quoted_string() {
-        assertEquals(singletonList("blind"), match("three {string} mice", "three \"blind\" mice"));
-    }
-
-    @Test
-    public void matches_multiple_double_quoted_strings() {
-        assertEquals(asList("blind", "crippled"),
-                match("three {string} and {string} mice", "three \"blind\" and \"crippled\" mice"));
-    }
-
-    @Test
-    public void matches_single_quoted_string() {
-        assertEquals(singletonList("blind"), match("three {string} mice", "three 'blind' mice"));
-    }
-
-    @Test
-    public void matches_multiple_single_quoted_strings() {
-        assertEquals(asList("blind", "crippled"),
-                match("three {string} and {string} mice", "three 'blind' and 'crippled' mice"));
-    }
-
-    @Test
-    public void does_not_match_misquoted_string() {
-        assertNull(match("three {string} mice", "three \"blind' mice"));
-    }
-
-    @Test
-    public void matches_single_quoted_string_with_double_quotes() {
-        assertEquals(singletonList("\"blind\""), match("three {string} mice", "three '\"blind\"' mice"));
-    }
-
-    @Test
-    public void matches_double_quoted_string_with_single_quotes() {
-        assertEquals(singletonList("'blind'"), match("three {string} mice", "three \"'blind'\" mice"));
-    }
-
-    @Test
-    public void matches_double_quoted_string_with_escaped_double_quote() {
-        assertEquals(singletonList("bl\"nd"), match("three {string} mice", "three \"bl\\\"nd\" mice"));
-    }
-
-    @Test
-    public void matches_single_quoted_string_with_escaped_single_quote() {
-        assertEquals(singletonList("bl'nd"), match("three {string} mice", "three 'bl\\'nd' mice"));
-    }
-
-    @Test
-    public void matches_single_quoted_empty_string_as_empty_string() {
-        assertEquals(singletonList(""), match("three {string} mice", "three '' mice"));
-    }
-
-    @Test
-    public void matches_double_quoted_empty_string_as_empty_string() {
-        assertEquals(singletonList(""), match("three {string} mice", "three \"\" mice"));
-    }
-
-    @Test
-    public void matches_single_quoted_empty_string_as_empty_string_along_with_other_strings() {
-        assertEquals(asList("", "handsome"), match("three {string} and {string} mice", "three '' and 'handsome' mice"));
-    }
-
-    @Test
-    public void matches_double_quoted_empty_string_as_empty_string_along_with_other_strings() {
-        assertEquals(asList("", "handsome"),
-                match("three {string} and {string} mice", "three \"\" and \"handsome\" mice"));
-    }
-
-    @Test
-    public void alternation_seperator_can_be_used_in_parameter() {
+    void alternation_separator_can_be_used_in_parameter() {
         parameterTypeRegistry
                 .defineParameterType(new ParameterType<>("a/b", "(.*)", String.class, (String arg) -> arg));
         assertEquals(singletonList("three mice"),
@@ -232,25 +70,7 @@ public class CucumberExpressionTest {
     }
 
     @Test
-    public void matches_escaped_parenthesis_1() {
-        assertEquals(emptyList(),
-                match("three \\(exceptionally) \\{string} mice", "three (exceptionally) {string} mice"));
-    }
-
-    @Test
-    public void matches_escaped_parenthesis_2() {
-        assertEquals(singletonList("blind"),
-                match("three \\((exceptionally)) \\{{string}} mice", "three (exceptionally) {\"blind\"} mice"));
-    }
-
-    @Test
-    public void matches_escaped_parenthesis_3() {
-        assertEquals(singletonList("blind"),
-                match("three \\((exceptionally)) \\{{string}} mice", "three (exceptionally) {\"blind\"} mice"));
-    }
-
-    @Test
-    public void matches_escaped_parenthesis_4() {
+    void matches_escaped_parenthesis_4() {
         parameterTypeRegistry
                 .defineParameterType(new ParameterType<>("{string}", "\"(.*)\"", String.class, (String arg) -> arg));
         assertEquals(singletonList("blind"),
@@ -258,156 +78,67 @@ public class CucumberExpressionTest {
     }
 
     @Test
-    public void matches_doubly_escaped_parenthesis() {
-        assertEquals(singletonList("blind"),
-                match("three \\\\(exceptionally) \\\\{string} mice", "three \\exceptionally \\\"blind\" mice"));
-    }
-
-    @Test
-    public void matches_escaped_slash() {
-        assertEquals(emptyList(), match("12\\/2020", "12/2020"));
-    }
-
-    @Test
-    public void matches_doubly_escaped_slash_1() {
-        assertEquals(emptyList(), match("12\\\\/2020", "12\\"));
-    }
-
-    @Test
-    public void matches_doubly_escaped_slash_2() {
-        assertEquals(emptyList(), match("12\\\\/2020", "2020"));
-    }
-
-    @Test
-    public void matches_int() {
-        assertEquals(singletonList(22), match("{int}", "22"));
-    }
-
-    @Test
-    public void doesnt_match_float_as_int() {
-        assertNull(match("{int}", "1.22"));
-    }
-
-    @Test
-    public void matches_float_1() {
-        assertEquals(singletonList(0.22f), match("{float}", "0.22"));
-    }
-
-    @Test
-    public void matches_float_2() {
-        assertEquals(singletonList(0.22f), match("{float}", ".22"));
-    }
-
-    @Test
-    public void matches_anonymous_parameter_type_with_hint() {
-        assertEquals(singletonList(0.22f), match("{}", "0.22", Float.class));
-    }
-
-    @Test
-    public void matches_anonymous_parameter_type() {
-        assertEquals(singletonList("0.22"), match("{}", "0.22"));
-    }
-
-    @Test
-    public void does_not_allow_parameter_type_with_left_bracket() {
-
-        final Executable testMethod = () -> match("{[string]}", "something");
-
-        final CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat("Unexpected message", thrownException.getMessage(),
-                is(equalTo("Illegal character '[' in parameter name {[string]}.")));
-    }
-
-    @Test
-    public void throws_unknown_parameter_type() {
-
-        final Executable testMethod = () -> match("{unknown}", "something");
-
-        final UndefinedParameterTypeException thrownException = assertThrows(UndefinedParameterTypeException.class,
-                testMethod);
-        assertThat(thrownException.getMessage(),
-                is(equalTo("Undefined parameter type {unknown}. Please register a ParameterType for {unknown}.")));
-    }
-
-    @Test
-    public void does_not_allow_optional_parameter_types() {
-
-        final Executable testMethod = () -> match("({int})", "3");
-
-        final CucumberExpressionException thrownException = assertThrows(CucumberExpressionException.class, testMethod);
-        assertThat(thrownException.getMessage(), is(equalTo("" +
-                "This Cucumber Expression has a problem at column 2:\n" +
-                "\n" +
-                "({int})\n" +
-                " ^---^\n" +
-                "An optional may not contain a parameter type.\n" +
-                "If you did not mean to use an parameter type you can use '\\{' to escape the the '{'")));
-    }
-
-    @Test
-    public void allows_escaped_optional_parameter_types() {
-        assertEquals(singletonList(3), match("\\({int})", "(3)"));
-    }
-
-    @Test
-    public void allows_parameter_type_in_alternation_1() {
-        assertEquals(singletonList(18), match("a/i{int}n/y", "i18n"));
-    }
-    @Test
-    public void allows_parameter_type_in_alternation_2() {
-        assertEquals(singletonList(11), match("a/i{int}n/y", "a11y"));
-    }
-
-    @Test
-    public void exposes_source() {
+    void exposes_source() {
         String expr = "I have {int} cuke(s)";
         assertEquals(expr, new CucumberExpression(expr, new ParameterTypeRegistry(Locale.ENGLISH)).getSource());
     }
 
     // Java-specific
+    @Test
+    void matches_anonymous_parameter_type_with_hint() {
+        assertEquals(singletonList(0.22f), match("{}", "0.22", Float.class));
+    }
 
     @Test
-    public void matches_byte() {
+    void documents_match_arguments() {
+        String expr = "I have {int} cuke(s)";
+        Expression expression = new CucumberExpression(expr, parameterTypeRegistry);
+        List<Argument<?>> args = expression.match("I have 7 cukes");
+        assertEquals(7, args.get(0).getValue());
+    }
+
+    @Test
+    void matches_byte() {
         assertEquals(singletonList(Byte.MAX_VALUE), match("{byte}", "127"));
     }
 
     @Test
-    public void matches_short() {
+    void matches_short() {
         assertEquals(singletonList(Short.MAX_VALUE), match("{short}", String.valueOf(Short.MAX_VALUE)));
     }
 
     @Test
-    public void matches_long() {
+    void matches_long() {
         assertEquals(singletonList(Long.MAX_VALUE), match("{long}", String.valueOf(Long.MAX_VALUE)));
     }
 
     @Test
-    public void matches_biginteger() {
+    void matches_biginteger() {
         BigInteger bigInteger = BigInteger.valueOf(Long.MAX_VALUE);
         bigInteger = bigInteger.pow(10);
         assertEquals(singletonList(bigInteger), match("{biginteger}", bigInteger.toString()));
     }
 
     @Test
-    public void matches_bigdecimal() {
+    void matches_bigdecimal() {
         BigDecimal bigDecimal = BigDecimal.valueOf(Math.PI);
         assertEquals(singletonList(bigDecimal), match("{bigdecimal}", bigDecimal.toString()));
     }
 
     @Test
-    public void matches_double_with_comma_for_locale_using_comma() {
+    void matches_double_with_comma_for_locale_using_comma() {
         List<?> values = match("{double}", "1,22", Locale.FRANCE);
         assertEquals(singletonList(1.22), values);
     }
 
     @Test
-    public void matches_float_with_zero() {
+    void matches_float_with_zero() {
         List<?> values = match("{float}", "0", Locale.ENGLISH);
         assertEquals(0.0f, values.get(0));
     }
 
     @Test
-    public void unmatched_optional_groups_have_null_values() {
+    void unmatched_optional_groups_have_null_values() {
         ParameterTypeRegistry parameterTypeRegistry = new ParameterTypeRegistry(Locale.ENGLISH);
         parameterTypeRegistry.defineParameterType(new ParameterType<>(
                 "textAndOrNumber",
--- cucumber-expressions/java/src/test/java/io/cucumber/cucumberexpressions/Expectation.java
+++ cucumber-expressions/java/src/test/java/io/cucumber/cucumberexpressions/Expectation.java
@@ -4,15 +4,16 @@ import java.util.Map;
 
 class Expectation {
     String expression;
+    String text;
     String expected;
     String exception;
 
-    Expectation(String expression, String expected, String exception) {
+    Expectation(String expression, String text, String expected, String exception) {
         this.expression = expression;
+        this.text = text;
         this.expected = expected;
         this.exception = exception;
     }
-
 
     public String getExpression() {
         return expression;
@@ -26,6 +27,7 @@ class Expectation {
     public static Expectation fromMap(Map<?, ?> map) {
         return new Expectation(
                 (String) map.get("expression"),
+                (String) map.get("text"),
                 (String) map.get("expected"),
                 (String) map.get("exception"));
     }
@@ -44,6 +46,14 @@ class Expectation {
 
     public void setExpected(String expected) {
         this.expected = expected;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 
 }
