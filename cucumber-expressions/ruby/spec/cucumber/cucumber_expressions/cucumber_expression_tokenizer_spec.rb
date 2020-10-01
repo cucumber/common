e48f75117c46df42503e261462881d9344b92381
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-10-01 12:43:01 +0100

    Update plan

--- cucumber-expressions/ruby/spec/cucumber/cucumber_expressions/cucumber_expression_tokenizer_spec.rb
+++ cucumber-expressions/ruby/spec/cucumber/cucumber_expressions/cucumber_expression_tokenizer_spec.rb
@@ -3,13 +3,17 @@ require 'json'
 require 'cucumber/cucumber_expressions/cucumber_expression_tokenizer'
 require 'cucumber/cucumber_expressions/errors'
 
+# Plan:
+# - tokenizer tests
+# - parser test
+# - cucumber expressions compiler
+# - phrases
+
 module Cucumber
   module CucumberExpressions
     describe 'Cucumber expression tokenizer' do
       Dir['testdata/tokens/*.yaml'].each do |testcase|
-
         expectation = YAML.load_file(testcase) # encoding?
-
         it "#{testcase}" do
           tokenizer = CucumberExpressionTokenizer.new
           if expectation['exception'].nil?
