dc87059d9e979c7826722e514619653f4e63263c
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-11-12 12:16:02 +0000

    Release messages v13.2.0

--- messages/elixir/mix.exs
+++ messages/elixir/mix.exs
@@ -2,7 +2,7 @@ defmodule CucumberMessages.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @vsn "0.1.0"
+  @vsn "13.2.0"
   @github "https://github.com/cucumber/cucumber/tree/master/messages/elixir"
   @name "CucumberMessages"
 
