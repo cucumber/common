b243e28ffed2cdfc6b990519a938f1099daac832
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-12-18 12:49:57 +0000

    Capture puppeteer errors

--- html-formatter/javascript/check.js
+++ html-formatter/javascript/check.js
@@ -3,7 +3,7 @@ const path = process.argv[2]
 const html = fs.readFileSync(path, 'utf-8')
 const puppeteer = require('puppeteer');
 
-(async () => {
+async function check() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setContent(html)
@@ -14,9 +14,10 @@ const puppeteer = require('puppeteer');
   });
   await browser.close();
 
-  if(!dynamicHTML) {
-    console.error(`The file ${path} did not render a .cucumber-react element. Inspect manually.`)
-    process.exit(1)
-  }
-  console.log(`${path} rendered OK!`)
-})();
+  if(!dynamicHTML) throw new Error(`The file ${path} did not render a .cucumber-react element. Inspect manually.`)
+}
+
+check().then(() => console.log(`${path} rendered OK!`)).catch((err) => {
+  console.error(err.stack)
+  process.exit(1)
+})
