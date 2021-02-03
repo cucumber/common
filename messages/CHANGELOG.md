6502a9724cc7b8ff7e1cd1c34087517452b5299b
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2021-02-03 13:03:48 +0000

    Default export does not load node.js streams. Fixes #1331

--- messages/CHANGELOG.md
+++ messages/CHANGELOG.md
@@ -11,6 +11,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ### Changed
 
+* [JavaScript] The stream classes have to be imported separately:
+  `import { MessageToBinaryStream, MessageToNdjsonStream, BinaryToMessageStream, NdjsonToMessageStream} from '@cucumber/messages/dist/src/stream'`
+  ([#1331](https://github.com/cucumber/cucumber/issues/1331))
+
 ### Deprecated
 
 ### Removed
--- messages/javascript/src/index.ts
+++ messages/javascript/src/index.ts
@@ -1,20 +1,7 @@
-import MessageToBinaryStream from './MessageToBinaryStream'
-import MessageToNdjsonStream from './MessageToNdjsonStream'
-import BinaryToMessageStream from './BinaryToMessageStream'
-import NdjsonToMessageStream from './NdjsonToMessageStream'
 import * as TimeConversion from './TimeConversion'
 import * as IdGenerator from './IdGenerator'
 import { io } from './messages'
 import messages = io.cucumber.messages
 import { version } from '../package.json'
 
-export {
-  messages,
-  MessageToBinaryStream,
-  MessageToNdjsonStream,
-  BinaryToMessageStream,
-  NdjsonToMessageStream,
-  TimeConversion,
-  IdGenerator,
-  version,
-}
+export { messages, TimeConversion, IdGenerator, version }
