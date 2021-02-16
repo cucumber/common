c79b86707b087f0ce009ac7107eb50dfe11a47dd
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-02-16 15:41:23 +0100

    [dotnet] improve ndjson serialization for tests

--- gherkin/dotnet/Gherkin.CLI/Program.cs
+++ gherkin/dotnet/Gherkin.CLI/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using Gherkin.Events;
 using Gherkin.Stream;
-using Newtonsoft.Json;
+using Utf8Json;
+using Utf8Json.Resolvers;
+using System.Text.RegularExpressions;
 
 namespace Gherkin.CLI
 {
@@ -40,17 +42,19 @@ namespace Gherkin.CLI
                 }
             }
 
-            var jsonSerializerSettings = new JsonSerializerSettings ();
-            jsonSerializerSettings.Formatting = Formatting.Indented;
-            jsonSerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-            jsonSerializerSettings.ContractResolver =  new FeatureAstJSonContractResolver();   
+            var resolver = CompositeResolver.Create(
+                new IJsonFormatter[] {},
+                new IJsonFormatterResolver[] { StandardResolver.ExcludeNullCamelCase }
+            );
 
             SourceEvents sourceEvents = new SourceEvents (paths);
             GherkinEvents gherkinEvents = new GherkinEvents (printSource, printAst, printPickles);
             foreach (var sourceEventEvent in sourceEvents) {
                 foreach (IEvent evt in gherkinEvents.Iterable(sourceEventEvent)) {
-                    Console.WriteLine (JsonConvert.SerializeObject (evt, jsonSerializerSettings));
-                    //Console.WriteLine (Utf8Json.JsonSerializer.ToJsonString((object)evt));
+                    var jsonString = Utf8Json.JsonSerializer.ToJsonString((object)evt, StandardResolver.ExcludeNullCamelCase);
+                    // manual way of ignoring empty arrays... This is not that easy with Utf8Json...
+                    jsonString = Regex.Replace(jsonString, @",?""\w+"":\s*\[\]", "");
+                    Console.WriteLine(jsonString);
                 }
             }
             return 0;
