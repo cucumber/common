ed73bd272b6702878230af07ac2097b87f6f5d0f
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2021-03-03 09:40:03 +0000

    Try to fix html-formatter build

--- html-formatter/javascript/check.js
+++ html-formatter/javascript/check.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
 const path = process.argv[2]
 const html = fs.readFileSync(path, 'utf-8')
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 async function check() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
   await page.setContent(html)
   const dynamicHTML = await page.evaluate(() => {
     const content = document.querySelector('.cucumber-react')
     return content && content.innerHTML
-  });
-  await browser.close();
+  })
+  await browser.close()
 
   if(!dynamicHTML) throw new Error(`The file ${path} did not render a .cucumber-react element. Inspect manually.`)
 }
