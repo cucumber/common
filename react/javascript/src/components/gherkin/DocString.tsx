3a0f40a867fea345496c797dfa2427f8fc6405aa
davidgoss <david@davidgoss.co>
2021-03-03 21:16:43 +0000

    use css module for doc string

--- react/javascript/src/components/gherkin/DocString.tsx
+++ react/javascript/src/components/gherkin/DocString.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { messages } from '@cucumber/messages'
 import HighLight from '../app/HighLight'
+import styles from './DocString.module.scss'
 
 interface IProps {
   docString: messages.GherkinDocument.Feature.Step.IDocString
@@ -8,7 +9,7 @@ interface IProps {
 
 const DocString: React.FunctionComponent<IProps> = ({ docString }) => {
   return (
-    <pre className="cucumber-docstring">
+    <pre className={styles.docstring}>
       <HighLight text={docString.content} />
     </pre>
   )
