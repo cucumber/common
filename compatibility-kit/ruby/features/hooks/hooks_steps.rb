eafe9dabe37b97e359d56a5fe8b1d65e707fb713
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-04-08 14:59:03 +0200

    Add test data to reproduce #1172

--- compatibility-kit/ruby/features/hooks/hooks_steps.rb
+++ compatibility-kit/ruby/features/hooks/hooks_steps.rb
@@ -1,3 +1,8 @@
+def attach_or_embed(world, data, media_type)
+  # Backward compatibility as the steps are also used by cucumber-ruby 3 which does not support `attach`
+  world.respond_to?(:attach) ? attach(data, media_type) : embed(data, media_type)
+end
+
 Before do
   # no-op
 end
@@ -16,4 +21,8 @@ end
 
 After('@some-tag or @some-other-tag') do
   raise StandardError, 'Exception in conditional hook'
+end
+
+After('@with-attachment') do
+  attach_or_embed(self, File.open("#{__dir__}/cucumber.svg"), 'image/svg+xml')
 end
