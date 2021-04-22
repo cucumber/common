da04f8589febae2e7725f0c67bf5be94f66372e7
cukebot <cukebot@cucumber.io>
2021-04-22 08:47:18 +0000

    Post release gherkin v18.1.1

--- gherkin/elixir/mix.exs
+++ gherkin/elixir/mix.exs
@@ -29,8 +29,8 @@ defmodule CucumberGherkin.MixProject do
   defp deps do
     [
       {:elixir_uuid, "~> 1.2"},
-      {:cucumber_messages, "~> 15.0"},
-      # {:cucumber_messages, path: "../../messages/elixir"},
+      # {:cucumber_messages, "~> 15.0"},
+      {:cucumber_messages, path: "../../messages/elixir"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
--- gherkin/go/go.mod
+++ gherkin/go/go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/stretchr/testify v1.7.0
 )
 
+replace github.com/cucumber/messages-go/v15 => ../../messages/go
+
 go 1.13
