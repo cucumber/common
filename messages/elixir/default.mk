3faef806b9c81447cb23d30bb54a2ea6f597f0b7
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-05-18 17:50:08 +0200

    make grammar file location configurable

--- messages/elixir/default.mk
+++ messages/elixir/default.mk
@@ -71,8 +71,9 @@ clean:
 ### COMMON stuff for all platforms
 
 BERP_VERSION = 1.3.0
+BERP_GRAMMAR = gherkin.berp
 
 define berp-generate-parser =
 -! dotnet tool list --tool-path /usr/bin | grep "berp\s*$(BERP_VERSION)" && dotnet tool update Berp --version $(BERP_VERSION) --tool-path /usr/bin
-berp -g gherkin.berp -t $< -o $@ --noBOM
+berp -g $(BERP_GRAMMAR) -t $< -o $@ --noBOM
 endef
