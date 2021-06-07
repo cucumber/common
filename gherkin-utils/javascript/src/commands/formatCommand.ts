5f1b7b91b63a146768984de6e5ee5b604802b658
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-06-07 14:37:04 +0100

    Format in any direction

--- gherkin-utils/javascript/src/commands/formatCommand.ts
+++ gherkin-utils/javascript/src/commands/formatCommand.ts
@@ -1,11 +1,14 @@
 import * as messages from '@cucumber/messages'
-import { AstBuilder, GherkinClassicTokenMatcher, Parser } from '@cucumber/gherkin'
+import { AstBuilder, GherkinClassicTokenMatcher, GherkinInMarkdownTokenMatcher, Parser } from '@cucumber/gherkin'
 import pretty from '../pretty'
 import { readFile, writeFile } from 'fs/promises'
 import path from "path";
 
-export default async (sourcePath: string, sourceDestination: string | undefined) => {
-  const tokenMatcher = new GherkinClassicTokenMatcher()
+export default async (sourcePath: string, destinationPath: string | undefined) => {
+  const sourceSyntax = path.extname(sourcePath) === '.feature' ? 'gherkin' : 'markdown'
+  const destinationSyntax = path.extname(destinationPath) === '.feature' ? 'gherkin' : 'markdown'
+
+  const tokenMatcher = sourceSyntax === 'gherkin' ? new GherkinClassicTokenMatcher() : new GherkinInMarkdownTokenMatcher()
   const newId = messages.IdGenerator.uuid()
   const parser = new Parser(new AstBuilder(newId), tokenMatcher)
 
@@ -13,7 +16,6 @@ export default async (sourcePath: string, sourceDestination: string | undefined)
   const gherkinDocument = parser.parse(gherkin)
   gherkinDocument.uri = sourcePath
 
-  const syntax = path.extname(sourceDestination) === '.feature' ? 'gherkin' : 'markdown'
-  const markdown = pretty(gherkinDocument, syntax)
-  await writeFile(sourceDestination, markdown, 'utf-8')
+  const markdown = pretty(gherkinDocument, destinationSyntax)
+  await writeFile(destinationPath, markdown, 'utf-8')
 }
