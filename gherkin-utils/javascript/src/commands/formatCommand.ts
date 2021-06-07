f37a8335827a35667680e7e23d01932475821e3d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-06-07 14:10:51 +0100

    Detect syntax from destination extension

--- gherkin-utils/javascript/src/commands/formatCommand.ts
+++ gherkin-utils/javascript/src/commands/formatCommand.ts
@@ -2,6 +2,7 @@ import * as messages from '@cucumber/messages'
 import { AstBuilder, GherkinClassicTokenMatcher, Parser } from '@cucumber/gherkin'
 import pretty from '../pretty'
 import { readFile, writeFile } from 'fs/promises'
+import path from "path";
 
 export default async (sourcePath: string, sourceDestination: string | undefined) => {
   const tokenMatcher = new GherkinClassicTokenMatcher()
@@ -12,6 +13,7 @@ export default async (sourcePath: string, sourceDestination: string | undefined)
   const gherkinDocument = parser.parse(gherkin)
   gherkinDocument.uri = sourcePath
 
-  const markdown = pretty(gherkinDocument, 'markdown')
+  const syntax = path.extname(sourceDestination) === '.feature' ? 'gherkin' : 'markdown'
+  const markdown = pretty(gherkinDocument, syntax)
   await writeFile(sourceDestination, markdown, 'utf-8')
 }
