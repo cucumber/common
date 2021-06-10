f740a1dcde58fa06fe251e2bb3fe7afe2fe5623f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-06-10 19:05:51 +0100

    Parse generated files before writing them out

--- gherkin-utils/javascript/src/commands/formatCommand.ts
+++ gherkin-utils/javascript/src/commands/formatCommand.ts
@@ -1,12 +1,7 @@
 import * as messages from '@cucumber/messages'
-import {
-  AstBuilder,
-  GherkinClassicTokenMatcher,
-  GherkinInMarkdownTokenMatcher,
-  Parser,
-} from '@cucumber/gherkin'
-import pretty, { Syntax } from '../pretty'
-import { readFile as readFileCb, writeFile as writeFileCb, unlink as unlinkCb } from 'fs'
+import {AstBuilder, GherkinClassicTokenMatcher, GherkinInMarkdownTokenMatcher, Parser,} from '@cucumber/gherkin'
+import pretty, {Syntax} from '../pretty'
+import {readFile as readFileCb, unlink as unlinkCb, writeFile as writeFileCb} from 'fs'
 import {promisify} from 'util'
 import path from 'path'
 import fg from 'fast-glob'
@@ -22,14 +17,36 @@ type Options = {
 }
 
 export default async (from: string, to: string | undefined, options: Partial<Options> = {}) => {
-  if(!to) {
+  if (!to) {
     to = from
   }
   const fromPaths = await fg(from)
   for (const fromPath of fromPaths) {
+    let gherkinDocument: messages.GherkinDocument
+    try {
+      gherkinDocument = await parseFile(fromPath)
+    } catch (err) {
+      console.error(`Failed to parse ${fromPath}`)
+      console.error(err.message)
+      process.exit(1)
+    }
+
     const toPath = makeToPath(fromPath, from, to)
-    await format(fromPath, toPath)
-    if(options.move && toPath !== fromPath) {
+    const destinationSyntax: Syntax = getSyntax(toPath)
+    const toSource = pretty(gherkinDocument, destinationSyntax)
+    try {
+      // Sanity check that what we generated is OK
+      makeParser(destinationSyntax).parse(toSource)
+    } catch (err) {
+      console.error(`Failed to generate ${fromPath} because the generated source is not valid.`)
+      console.error(`Please report a bug at https://github.com/cucumber/common/issues/new`)
+      console.error(err.message)
+      console.error(`--- Generated ${destinationSyntax} source ---`)
+      console.error(toSource)
+      process.exit(2)
+    }
+    await writeFile(toPath, toSource, 'utf-8')
+    if (options.move && toPath !== fromPath) {
       await unlink(fromPath)
     }
   }
@@ -43,21 +60,24 @@ export function makeToPath(fromPath: string, from: string, to: string): string {
   return to.replace(/\*\*?/g, () => fromMatches.shift())
 }
 
-async function format(fromPath: string, toPath: string) {
-  const sourceSyntax: Syntax = path.extname(fromPath) === '.feature' ? 'gherkin' : 'markdown'
-  const destinationSyntax: Syntax = path.extname(toPath) === '.feature' ? 'gherkin' : 'markdown'
+async function parseFile(fromPath: string) {
+  const fromParser = makeParser(getSyntax(fromPath))
+  const fromSource = await readFile(fromPath, 'utf-8')
+  const gherkinDocument = fromParser.parse(fromSource)
+  gherkinDocument.uri = fromPath
+  return gherkinDocument;
+}
 
-  const parser = new Parser(
+
+function getSyntax(fromPath: string) {
+  return path.extname(fromPath) === '.feature' ? 'gherkin' : 'markdown';
+}
+
+function makeParser(sourceSyntax: "markdown" | "gherkin") {
+  return new Parser(
     new AstBuilder(messages.IdGenerator.uuid()),
     sourceSyntax === 'gherkin'
       ? new GherkinClassicTokenMatcher()
       : new GherkinInMarkdownTokenMatcher()
-  )
-
-  const fromSource = await readFile(fromPath, 'utf-8')
-  const gherkinDocument = parser.parse(fromSource)
-  gherkinDocument.uri = fromPath
-
-  const toSource = pretty(gherkinDocument, destinationSyntax)
-  await writeFile(toPath, toSource, 'utf-8')
+  );
 }
