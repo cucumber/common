8310bfa2eb5e3e076be2aee08ad3d064ef541f07
davidgoss <david@davidgoss.co>
2021-06-14 15:53:42 +0100

    try imperative import of global-jsdom

--- react/javascript/test/components/utils.tsx
+++ react/javascript/test/components/utils.tsx
@@ -5,6 +5,7 @@ import { Query as CucumberQuery } from '@cucumber/query'
 import UriContext from '../../src/UriContext'
 import CucumberQueryContext from '../../src/CucumberQueryContext'
 import GherkinQueryContext from '../../src/GherkinQueryContext'
+import 'global-jsdom/register'
 
 export interface TestRenderOptions {
   uri?: string
