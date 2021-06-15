9f7cea8c3953866b42cbe90b2e9cc24fc99a6fef
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-06-15 11:46:32 +0200

    Add some rdoc to Messages::Message::Serialization

--- messages/ruby/lib/cucumber/messages/message/serialization.rb
+++ messages/ruby/lib/cucumber/messages/message/serialization.rb
@@ -1,4 +1,5 @@
 require 'cucumber/messages/message/utils'
+require 'json'
 
 module Cucumber
   module Messages
@@ -6,6 +7,19 @@ module Cucumber
       include Cucumber::Messages::Message::Utils
 
       module Serialization
+
+        ##
+        # Returns a new Hash formed from the message attributes
+        # If +camelize:+ keyword parameter is set to true, then keys will be camelized
+        #
+        #   Cucumber::Messages::Duration.new(seconds: 1, nanos: 42).to_h                                 # => { seconds: 1, nanos: 42 }
+        #   Cucumber::Messages::PickleTag.new(name: 'foo', ast_node_id: 'abc-def').to_h(camelize: true)  # => { name: 'foo', astNodeId: 'abc-def' }
+        #
+        # It is recursive so embedded messages are also processed
+        #
+        #   location = Cucumber::Messages::Location.new(line: 2)
+        #   Cucumber::Messages::Comment.new(location: location, text: 'comment').to_h  # => { location: { line: 2, :column: nil }, text: "comment" }
+        #
 
         def to_h(camelize: false)
           self.instance_variables.to_h do |variable_name|
@@ -15,6 +29,19 @@ module Cucumber
             [key.to_sym, get_h_value(variable_name, camelize: camelize)]
           end
         end
+
+        ##
+        # Generates a JSON document from the message.
+        # Keys are camelized during the process
+        #
+        #   Cucumber::Messages::Duration.new(seconds: 1, nanos: 42).to_json                 # => '{"seconds":1,"nanos":42}'
+        #   Cucumber::Messages::PickleTag.new(name: 'foo', ast_node_id: 'abc-def').to_json  # => '{"name":"foo","astNodeId":"abc-def"}'
+        #
+        # As #to_h, the method is recursive
+        #
+        #   location = Cucumber::Messages::Location.new(line: 2)
+        #   Cucumber::Messages::Comment.new(location: location, text: 'comment').to_json     # => '{"location":{"line":2,"column":null},"text":"comment"}'
+        #
 
         def to_json
           to_h(camelize: true).to_json
