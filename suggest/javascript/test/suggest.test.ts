ed75417899322c53938c053f041c6f053e624def
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-07-11 13:47:58 +0100

    Add first test

---
+++ suggest/javascript/test/suggest.test.ts
@@ -0,0 +1,19 @@
+import assert from 'assert'
+
+type Suggestion = {}
+
+function suggest(text: string): readonly Suggestion[] {
+  return [{
+    text: 'I have 42 cukes in my belly'
+  }]
+}
+
+describe('suggest', () => {
+  it('suggests an existing step', () => {
+    const suggestions = suggest('I have')
+    const expectedSuggestion: Suggestion = {
+      text: 'I have 42 cukes in my belly'
+    }
+    assert.deepStrictEqual(suggestions, [expectedSuggestion])
+  })
+})
