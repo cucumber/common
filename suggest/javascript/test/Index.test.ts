311d2c30a53644bd4a23b71d6ffe87415ab46349
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-07-12 22:35:31 +0100

    Add a performance test

--- suggest/javascript/test/Index.test.ts
+++ suggest/javascript/test/Index.test.ts
@@ -3,36 +3,63 @@ import bruteForceIndex from '../src/bruteForceIndex'
 import { Index, PermutationExpression } from '../src/types'
 import fuseIndex from './fuseIndex'
 import jsSearchIndex from './jsSearchIndex'
+import * as txtgen from 'txtgen'
 
 type BuildIndex = (permutationExpressions: readonly PermutationExpression[]) => Index
 
 function verifyIndexContract(name: string, buildIndex: BuildIndex) {
   describe(name, () => {
-    const exp1 = ['I have ', ['42', '98'], ' cukes in my belly']
-    const exp2 = ['I am a teapot']
-    let index: Index
-    beforeEach(() => {
-      index = buildIndex([exp1, exp2])
+    describe('basics', () => {
+      const exp1 = ['I have ', ['42', '98'], ' cukes in my belly']
+      const exp2 = ['I am a teapot']
+      let index: Index
+      beforeEach(() => {
+        index = buildIndex([exp1, exp2])
+      })
+
+      it('matches two words in the beginning of an expression', () => {
+        const suggestions = index('have')
+        assert.deepStrictEqual(suggestions, [exp1])
+      })
+
+      it('matches a word in an expression', () => {
+        const suggestions = index('cukes')
+        assert.deepStrictEqual(suggestions, [exp1])
+      })
+
+      it('matches a word in a choice', () => {
+        const suggestions = index('98')
+        assert.deepStrictEqual(suggestions, [exp1])
+      })
+
+      it('matches nothing', () => {
+        const suggestions = index('nope')
+        assert.deepStrictEqual(suggestions, [])
+      })
     })
 
-    it('matches two words in the beginning of an expression', () => {
-      const suggestions = index('I have')
-      assert.deepStrictEqual(suggestions, [exp1])
-    })
+    describe('performance', () => {
+      it('matches how quickly exactly?', () => {
+        for (let i = 0; i < 100; i++) {
+          const length = 100
+          const expressions: PermutationExpression[] = Array(length).fill(0).map(() => [txtgen.sentence()])
+          const index = buildIndex(expressions)
 
-    it('matches a word in an expression', () => {
-      const suggestions = index('cukes')
-      assert.deepStrictEqual(suggestions, [exp1])
-    })
+          const sentence = expressions[Math.floor(length/2)][0] as string
+          const words = sentence.split(' ')
+          // Find a word longer than 5 letters (fall back to the middle word if there are none)
+          const term = words.find(word => word.length > 5) || words[Math.floor(words.length / 2)]
 
-    it('matches a word in a choice', () => {
-      const suggestions = index('98')
-      assert.deepStrictEqual(suggestions, [exp1])
-    })
-
-    it('matches nothing', () => {
-      const suggestions = index('nope')
-      assert.deepStrictEqual(suggestions, [])
+          const suggestions = index(term)
+          assert(suggestions.length > 0)
+          for (const suggestion of suggestions) {
+            const s = expressions[Math.floor(length/2)][0] as string
+            if(!s.includes(term)) {
+              console.error(`WARNING: ${name} - "${s}" does not include "${term}"`)
+            }
+          }
+        }
+      })
     })
   })
 }
