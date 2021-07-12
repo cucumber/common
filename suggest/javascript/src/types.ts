fe2c64355fb12118cd09343edc4a97d66efa33d8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-07-13 00:16:31 +0100

    Fix typos

--- suggest/javascript/src/types.ts
+++ suggest/javascript/src/types.ts
@@ -2,7 +2,7 @@
  * A search index function.
  *
  * @param text a text to search for
- * @return results in the form of permutation expressions
+ * @return results in the form of step documents
  */
 export type Index = (text: string) => readonly StepDocument[]
 
