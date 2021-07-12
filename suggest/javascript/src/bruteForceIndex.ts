7a6ce3d13a3b879a591146cc0213f8889166187b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-07-12 16:30:10 +0100

    Add contract test for index

--- suggest/javascript/src/bruteForceIndex.ts
+++ suggest/javascript/src/bruteForceIndex.ts
@@ -1,11 +1,17 @@
 import { Index, PermutationExpression } from './types'
 
+/**
+ * A brute force (not very performant) index that matches permutation expressions with string.includes()
+ *
+ * @param permutationExpressions
+ */
 export default function bruteForceIndex(permutationExpressions: readonly PermutationExpression[]): Index {
   return (text) => {
-    return permutationExpressions.filter(permutationExpression => hasWordStartingWith(permutationExpression, text))
+    const predicate = (segment: string) => segment.includes(text)
+    return permutationExpressions.filter(permutationExpression => matches(permutationExpression, predicate))
   }
 }
 
-function hasWordStartingWith(expression: PermutationExpression, text: string) {
-  return true
+function matches(expression: PermutationExpression, predicate: (segment: string) => boolean): boolean {
+  return !!expression.find(segment => typeof segment === 'string' ? predicate(segment) : !!segment.find(predicate))
 }
--- suggest/javascript/test/Index.test.ts
+++ suggest/javascript/test/Index.test.ts
@@ -1,11 +1,36 @@
 import assert from 'assert'
 import bruteForceIndex from '../src/bruteForceIndex'
+import { Index, PermutationExpression } from '../src/types'
 
-describe('Index', () => {
-  it('matches', () => {
-    let exp1 = ['I have ', ['42', '98'], ' cukes in my belly']
-    const index = bruteForceIndex([exp1])
-    const suggestions = index('I have')
-    assert.deepStrictEqual(suggestions, [exp1])
+type BuildIndex = (permutationExpressions: readonly PermutationExpression[]) => Index
+
+function verifyIndexContract(name: string, buildIndex: BuildIndex) {
+  describe(name, () => {
+    it('matches the beginning of an expression', () => {
+      let exp1 = ['I have ', ['42', '98'], ' cukes in my belly']
+      let exp2 = ['I am a teapot']
+      const index = buildIndex([exp1, exp2])
+      const suggestions = index('I have')
+      assert.deepStrictEqual(suggestions, [exp1])
+    })
+
+    it('matches a word in an expression', () => {
+      let exp1 = ['I have ', ['42', '98'], ' cukes in my belly']
+      let exp2 = ['I am a teapot']
+      const index = buildIndex([exp1, exp2])
+      const suggestions = index('cukes')
+      assert.deepStrictEqual(suggestions, [exp1])
+    })
+
+    it('matches a word in an choice', () => {
+      let exp1 = ['I have ', ['42', '98'], ' cukes in my belly']
+      let exp2 = ['I am a teapot']
+      const index = buildIndex([exp1, exp2])
+      const suggestions = index('98')
+      assert.deepStrictEqual(suggestions, [exp1])
+    })
   })
-})
+}
+
+verifyIndexContract('bruteForceIndex', (expressions) => bruteForceIndex(expressions))
+// TODO: Add a more performant index
