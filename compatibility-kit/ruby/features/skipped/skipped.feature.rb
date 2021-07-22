a2f01debeff0b42da15d2a9dd922b60cab1ba034
David Goss <david@davidgoss.co>
2021-07-22 19:34:06 +0100

    cck: add cases for pending, skipped and undefined (#1659)
    * add skipped to cck as-is
    
    * implement skipped in fake-cucumber, regenerate cck
    
    * add one for pending
    
    * improve words and tweak some details
    
    * add undefined
    
    * add to storybook
    
    * add ruby support code
    
    * ruby doesnt support pending yet
    
    * ruby also doesnt support skipped?
    
    * incredible
    
    * add changelog entry
    
    * rework steps in cck features

---
+++ compatibility-kit/ruby/features/skipped/skipped.feature.rb
@@ -0,0 +1,15 @@
+After('@skip') do
+  'skipped'
+end
+
+Given('an implemented step') do
+  # no-op
+end
+
+Given('a step that we expect to be skipped') do
+  # no-op
+end
+
+Given('a step that skips') do
+  'skipped'
+end
