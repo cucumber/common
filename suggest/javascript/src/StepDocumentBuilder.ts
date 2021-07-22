e6fb90c779b75ddcc6af4180798a828c0ebb24a5
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-07-22 07:50:13 -0500

    Remove old code

--- suggest/javascript/src/StepDocumentBuilder.ts
+++ suggest/javascript/src/StepDocumentBuilder.ts
@@ -5,28 +5,20 @@ type TextOrParameterTypeNameExpression = TextOrParameterTypeNameSegment[]
 type TextOrParameterTypeNameSegment = string | ParameterTypeData
 type ParameterTypeData = { name: string, regexpStrings: string }
 
-type TextOrIndexExpression = TextOrIndexSegment[]
-type TextOrIndexSegment = string | number
-type ChoicesArray = Set<string>[]
-
 export default class StepDocumentBuilder {
   // We can't store StepDocument in a Set, so we store a string representation instead
-  private readonly jsonDocuments = new Set<string>()
   private readonly jsonTextOrParameterTypeNameExpression = new Set<string>()
-  private choicesArray: ChoicesArray = []
   private parameterTypes: ParameterType<any>[] = []
 
   constructor(private readonly expression: Expression, private readonly choicesByParameterTypeRegexpStrings: Map<string,Set<string>>) {}
 
   update(text: string) {
     const args = this.expression.match(text)
-    const firstUpdate = this.choicesArray.length === 0
     if (args) {
+      const firstUpdate = this.jsonTextOrParameterTypeNameExpression.size === 0
       if (firstUpdate) {
-        this.choicesArray = args.map(() => new Set())
         this.parameterTypes = args.map(arg => arg.getParameterType())
       }
-      const textOrIndexExpression: TextOrIndexExpression = []
       const textOrParameterTypeNameExpression: TextOrParameterTypeNameExpression = []
       let index = 0
       for (let argIndex = 0; argIndex < args.length; argIndex++) {
@@ -37,25 +29,19 @@ export default class StepDocumentBuilder {
         const parameterType = this.parameterTypes[argIndex]
         const regexpStrings = parameterType.regexpStrings.join('|')
         textOrParameterTypeNameExpression.push({ name: parameterType.name || '', regexpStrings })
-        let choices2 = this.choicesByParameterTypeRegexpStrings.get(regexpStrings)
-        if(!choices2) {
-          choices2 = new Set<string>()
-          this.choicesByParameterTypeRegexpStrings.set(regexpStrings, choices2)
+        let choices = this.choicesByParameterTypeRegexpStrings.get(regexpStrings)
+        if(!choices) {
+          choices = new Set<string>()
+          this.choicesByParameterTypeRegexpStrings.set(regexpStrings, choices)
         }
-        choices2.add(arg.group.value)
-
-        textOrIndexExpression.push(segment)
-        textOrIndexExpression.push(argIndex)
-        const choices = this.choicesArray[argIndex]
         choices.add(arg.group.value)
+
         index = arg.group.end
       }
       const lastSegment = text.substring(index)
       if (lastSegment !== '') {
-        textOrIndexExpression.push(lastSegment)
         textOrParameterTypeNameExpression.push(lastSegment)
       }
-      this.jsonDocuments.add(JSON.stringify(textOrIndexExpression))
       this.jsonTextOrParameterTypeNameExpression.add(JSON.stringify(textOrParameterTypeNameExpression))
     }
   }
