63e1ab7149530da5bbcc7f26f1044f60ca4cfe4e
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-08-11 07:47:22 +0100

    Fix test

--- lsp/javascript/src/writeStepDocumentsJson.ts
+++ lsp/javascript/src/writeStepDocumentsJson.ts
@@ -1,24 +1,51 @@
-import { ChildProcess } from 'child_process'
-import { pipeline as pipelineCb, Transform } from 'stream'
-import { promisify } from 'util'
+import { spawn } from 'child_process'
+import { pipeline, Transform, Writable } from 'stream'
 import { NdjsonToMessageStream } from '@cucumber/message-streams'
 import { StepDocument } from '@cucumber/suggest'
 import StepDocumentsStream from '../src/StepDocumentsStream'
 import fs from 'fs'
 
-const pipeline = promisify(pipelineCb)
+export default function writeStepDocumentsJson(command: string, args: string[], jsonPath: string): Promise<void> {
+  const cucumber = spawn(command, args)
+  const stderr: Buffer[] = []
 
-export default async function writeStepDocumentsJson(cucumber: ChildProcess, jsonPath: string) {
-  await pipeline(
-    cucumber.stdout,
-    new NdjsonToMessageStream(),
-    new StepDocumentsStream(),
-    new Transform({
-      objectMode: true,
-      transform(stepDocuments: StepDocument[], encoding, callback) {
-        callback(null, JSON.stringify(stepDocuments, null, 2))
+  return new Promise((resolve, reject) => {
+    cucumber.on('error', reject)
+    cucumber.on('close', (code, signal) => {
+      if (signal) {
+        reject(new Error(`Received signal ${signal}`))
       }
-    }),
-    fs.createWriteStream(jsonPath, { encoding: 'utf-8' })
-  )
+      if (code === 0) {
+        return resolve()
+      }
+      reject(new Error(`Exited with status ${code}. STDERR:\n${Buffer.concat(stderr).toString('utf8')}`))
+    })
+
+    cucumber.on('spawn', () => {
+      pipeline(
+        cucumber.stderr,
+        new Writable({
+          write(chunk: Buffer, _, callback) {
+            stderr.push(chunk)
+            callback()
+          }
+        }),
+        (err) => err && reject(err)
+      )
+
+      pipeline(
+        cucumber.stdout,
+        new NdjsonToMessageStream(),
+        new StepDocumentsStream(),
+        new Transform({
+          objectMode: true,
+          transform(stepDocuments: StepDocument[], encoding, callback) {
+            callback(null, JSON.stringify(stepDocuments, null, 2))
+          }
+        }),
+        fs.createWriteStream(jsonPath, { encoding: 'utf-8' }),
+        (err) => err ? reject(err) : resolve()
+      )
+    })
+  })
 }
--- lsp/javascript/test/writeStepDocuments.test.ts
+++ lsp/javascript/test/writeStepDocuments.test.ts
@@ -1,4 +1,3 @@
-import { spawn } from 'child_process'
 import assert from 'assert'
 import writeStepDocumentsJson from '../src/writeStepDocumentsJson'
 import withTempFile from './withTempFile'
@@ -6,9 +5,9 @@ import fs from 'fs'
 
 describe('cucumberDryRunMessages', () => {
   it('writes a step documents JSON file with spawn', async () => {
-    const cucumber = spawn('npm', ['run', '--silent', 'cucumber', '--', '--profile', 'lsp'])
     await withTempFile(async (jsonPath) => {
-      await writeStepDocumentsJson(cucumber, jsonPath)
+      const messages = fs.readFileSync(__dirname + '/fixtures/suggest.ndjson', 'utf-8')
+      await writeStepDocumentsJson('echo', [messages], jsonPath)
       const json = fs.readFileSync(jsonPath, { encoding: 'utf-8' })
       assert.deepStrictEqual(JSON.parse(json).length, 7)
     })
