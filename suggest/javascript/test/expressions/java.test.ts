3136610e4d1d43f4099fbe6084350a89d07e9766
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-09-20 15:18:54 +0100

    Tree-sitter is working

--- suggest/javascript/test/expressions/java.test.ts
+++ suggest/javascript/test/expressions/java.test.ts
@@ -1,38 +1,59 @@
-import { ParameterTypeRegistry } from '@cucumber/cucumber-expressions'
 import Parser from 'web-tree-sitter'
 import assert from 'assert'
-import { addJavaExpressions } from '../../src/expressions/java'
+import { buildExpressions } from '../../src/expressions/java'
 
-describe('addExpressions', () => {
+describe('buildExpressions', () => {
   let parser: Parser
   let Java: Parser.Language
-  let parameterTypeRegistry: ParameterTypeRegistry
 
   beforeEach(async () => {
     await Parser.init()
     parser = new Parser()
     Java = await Parser.Language.load('tree-sitter-java.wasm')
     parser.setLanguage(Java)
-
-    parameterTypeRegistry = new ParameterTypeRegistry()
   })
 
   it('builds expressions from .java source', async () => {
-    const source = `
+    const stepdefs = `
 class StepDefinitions {
     @Given("I have {int} cukes in my belly"  )
-    void function() {
+    void method1() {
     }
 
     @When(  "you have some time")
-    void function2() {
+    void method2() {
+    }
+
+    @Then("a {iso-date}")
+    void method3() {
+    }
+
+    @Then("a {date}")
+    void method4() {
     }
 }
 `
-    const expressions = await addJavaExpressions([], parser, Java, parameterTypeRegistry, source)
+
+    const parameterTypes = `
+class ParameterTypes {
+    @ParameterType("(?:.*) \\\\d{1,2}, \\\\d{4}")
+    public Date date(String date) throws ParseException {
+        return getDateInstance(MEDIUM, ENGLISH).parse(date);
+    }
+
+    @ParameterType(name = "iso-date", value = "\\\\d{4}-\\\\d{2}-\\\\d{2}")
+    public Date isoDate(String date) throws ParseException {
+        return new SimpleDateFormat("yyyy-mm-dd").parse(date);
+    }
+}
+`
+
+    const expressions = buildExpressions(parser, Java, [stepdefs, parameterTypes])
     assert.deepStrictEqual(expressions.map(e => e.source), [
       'I have {int} cukes in my belly',
-      'you have some time'
+      'you have some time',
+      'a {iso-date}',
+      'a {date}',
     ])
   })
 })
