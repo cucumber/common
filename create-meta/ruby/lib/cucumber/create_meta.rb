ee036d94fb490f36e7fd0114393cb01307db5252
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-10-01 09:35:07 +0200

    Create messages rather than plain hashes

--- create-meta/ruby/lib/cucumber/create_meta.rb
+++ create-meta/ruby/lib/cucumber/create_meta.rb
@@ -10,25 +10,25 @@ module Cucumber
     CI_DICT = JSON.parse(IO.read(File.join(File.dirname(__FILE__), "ciDict.json")))
 
     def create_meta(tool_name, tool_version, env = ENV)
-      {
-          protocol_version: Cucumber::Messages::VERSION,
-          implementation: {
-              name: tool_name,
-              version: tool_version
-          },
-          runtime: {
-              name: RUBY_ENGINE,
-              version: RUBY_VERSION
-          },
-          os: {
-              name: RbConfig::CONFIG['target_os'],
-              version: Sys::Uname.uname.version
-          },
-          cpu: {
-              name: RbConfig::CONFIG['target_cpu']
-          },
-          ci: detect_ci(env)
-      }
+      Cucumber::Messages::Meta.new(
+        protocol_version: Cucumber::Messages::VERSION,
+        implementation: Cucumber::Messages::Product.new(
+            name: tool_name,
+            version: tool_version
+        ),
+        runtime: Cucumber::Messages::Product.new(
+            name: RUBY_ENGINE,
+            version: RUBY_VERSION
+        ),
+        os: Cucumber::Messages::Product.new(
+            name: RbConfig::CONFIG['target_os'],
+            version: Sys::Uname.uname.version
+        ),
+        cpu: Cucumber::Messages::Product.new(
+            name: RbConfig::CONFIG['target_cpu']
+        ),
+        ci: detect_ci(env)
+      )
     end
 
     def detect_ci(env)
@@ -43,17 +43,17 @@ module Cucumber
       url = evaluate(ci_system['url'], env)
       return nil if url.nil?
 
-      {
-          url: url,
-          name: ci_name,
-          buildNumber: evaluate(ci_system['buildNumber'], env),
-          git: {
-              remote: remove_userinfo_from_url(evaluate(ci_system['git']['remote'], env)),
-              revision: evaluate(ci_system['git']['revision'], env),
-              branch: evaluate(ci_system['git']['branch'], env),
-              tag: evaluate(ci_system['git']['tag'], env),
-          }.delete_if {|k,v| v.nil?}
-      }
+      Cucumber::Messages::Ci.new(
+        url: url,
+        name: ci_name,
+        build_number: evaluate(ci_system['buildNumber'], env),
+        git: Cucumber::Messages::Git.new(
+          remote: remove_userinfo_from_url(evaluate(ci_system['git']['remote'], env)),
+          revision: evaluate(ci_system['git']['revision'], env),
+          branch: evaluate(ci_system['git']['branch'], env),
+          tag: evaluate(ci_system['git']['tag'], env),
+        )
+      )
     end
 
     def remove_userinfo_from_url(value)
