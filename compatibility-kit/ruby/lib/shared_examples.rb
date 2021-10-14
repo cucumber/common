add619bfa5c2901c153a47c66a5308a03df9891c
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-10-14 11:16:19 +0200

    Refactor shared examples

--- compatibility-kit/ruby/lib/shared_examples.rb
+++ compatibility-kit/ruby/lib/shared_examples.rb
@@ -5,12 +5,47 @@ require 'cucumber/messages'
 require 'messages_comparator'
 require 'keys_checker'
 
-def message_type(message)
-  message.to_h.keys.first
+RSpec.shared_examples 'equivalent messages' do
+  # Note: to use those examples, you need to define:
+  # let(:example) {  } # the name of the example to test
+  # let(:messages) {  } # the messages to validate
+
+  let(:example) { raise "`example` missing: add `let(:example) { example_name }` to your spec" }
+  let(:messages) { raise "`messages` missing: add `let(:messages) { ndjson }` to your spec" }
+
+  let(:example_path) { Cucumber::CompatibilityKit.getExamplePath(example) }
+
+  let(:parsed_original) { parse_ndjson_file("#{example_path}/#{example}.feature.ndjson") }
+  let(:parsed_generated) { parse_ndjson(messages) }
+
+  let(:original_messages_types) { parsed_original.map { |msg| message_type(msg) } }
+  let(:generated_messages_types) { parsed_generated.map { |msg| message_type(msg) } }
+
+  it 'generates valid message types' do
+    debug_lists(original_messages_types, generated_messages_types)
+    expect(generated_messages_types).to contain_exactly(*original_messages_types)
+  end
+
+  it 'generates valid message structure' do
+    comparator = CCK::MessagesComparator.new(CCK::KeysChecker, parsed_generated, parsed_original)
+    comparator.debug if ENV['VERBOSE']
+
+    if comparator.errors.any?
+      fail "There were comparison errors: #{comparator.errors}"
+    end
+  end
 end
 
-def parse_ndjson(path)
-  Cucumber::Messages::NdjsonToMessageEnumerator.new(File.read(path))
+def parse_ndjson_file(path)
+  parse_ndjson(File.read(path))
+end
+
+def parse_ndjson(ndjson)
+  Cucumber::Messages::NdjsonToMessageEnumerator.new(ndjson)
+end
+
+def message_type(message)
+  message.to_h.keys.first
 end
 
 def debug_lists(expected, obtained)
@@ -24,31 +59,5 @@ def debug_lists(expected, obtained)
   to_read.each_with_index do |_, index|
     ok = expected[index] == obtained[index] ? 'v' : 'x'
     puts "[#{ok}] | #{expected[index]} #{columnize} #{obtained[index]}"
-  end
-end
-
-RSpec.shared_examples 'equivalent messages' do
-  # Note: to use those examples, you need to define:
-  # let(:original) { 'path to .ndjson file in CCK' }
-  # let(:generated) { 'path to generated .ndjson file' }
-
-  let(:parsed_original) { parse_ndjson("#{File.expand_path(File.dirname(__FILE__))}/../features/#{original}/#{original}.feature.ndjson") }
-  let(:parsed_generated) { parse_ndjson(generated) }
-
-  let(:original_messages_types) { parsed_original.map { |msg| message_type(msg) } }
-  let(:generated_messages_types) { parsed_generated.map { |msg| message_type(msg) } }
-
-  it 'identical message types' do
-    debug_lists(original_messages_types, generated_messages_types)
-    expect(generated_messages_types).to contain_exactly(*original_messages_types)
-  end
-
-  it 'identical individual message structure' do
-    comparator = CCK::MessagesComparator.new(CCK::KeysChecker, parsed_generated, parsed_original)
-    comparator.debug if ENV['VERBOSE']
-
-    if comparator.errors.any?
-      fail "There were comparison errors: #{comparator.errors}"
-    end
   end
 end
