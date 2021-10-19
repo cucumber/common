84e55491642895c21b58ea4ed8c16efc1b0736cf
Aurélien Reeves <aurelien.reeves@smartbear.com>
2021-10-19 16:47:39 +0200

    [Cck][ruby] Ignore messages related to an actual CI (#1803)
    * Update KeysChecker to ignores actual CI related messages
    
    * Update MessagesComparator to ignores actual CI related messages
    
    * Update CHANGELOG.md

--- compatibility-kit/ruby/lib/keys_checker.rb
+++ compatibility-kit/ruby/lib/keys_checker.rb
@@ -14,11 +14,15 @@ module CCK
 
       missing_keys = (expected_keys - found_keys)
 
-      extra_keys = (found_keys - expected_keys)
+      extra_keys = (found_keys - expected_keys).reject { |key|
+        ENV['CI'] && found.class == Cucumber::Messages::Meta && key == :ci
+      }
 
       errors << "Found extra keys in message #{found.class.name}: #{extra_keys}" unless extra_keys.empty?
       errors << "Missing keys in message #{found.class.name}: #{missing_keys}" unless missing_keys.empty?
       errors
+    rescue StandardError => e
+      ["Unexpected error: #{e.message}"]
     end
   end
 end
