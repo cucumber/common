c692a4a76d1b6b3696b5762b54c2a8414d8383d0
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-10-19 14:20:10 +0200

    Update KeysChecker to ignores actual CI related messages

--- compatibility-kit/ruby/spec/keys_checker_spec.rb
+++ compatibility-kit/ruby/spec/keys_checker_spec.rb
@@ -73,5 +73,34 @@ describe CCK::KeysChecker do
         expect(subject.compare(default_set, default_not_set)).to be_empty
       end
     end
+
+    context 'when executed as part of a CI' do
+      before do
+        allow(ENV).to receive(:[]).with('CI').and_return(true)
+      end
+
+      it 'ignores actual CI related messages' do
+        found = Cucumber::Messages::Meta.new(
+          ci: Cucumber::Messages::Ci.new(name: 'Some CI')
+        )
+
+        expected = Cucumber::Messages::Meta.new
+
+        expect(subject.compare(found, expected)).to be_empty
+      end
+    end
+
+    context 'when an unexcpected error occurs' do
+      it 'does not raise error' do
+        expect {
+          subject.compare(nil, nil)
+        }.not_to raise_error
+      end
+
+      it 'returns the error' do
+        expect(subject.compare(nil, nil))
+          .to eq(['Unexpected error: wrong number of arguments (given 1, expected 0)'])
+      end
+    end
   end
 end
