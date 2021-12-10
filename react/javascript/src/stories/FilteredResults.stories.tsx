8484772ccc1f4da6709a1e8257bd28ca4d9e3537
David Goss <david@davidgoss.co>
2021-12-10 01:41:43 +0000

    start adding FilteredResults stories

---
+++ react/javascript/src/stories/FilteredResults.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import * as messages from '@cucumber/messages'
+import { Query as GherkinQuery } from '@cucumber/gherkin-utils'
+import { Query as CucumberQuery } from '@cucumber/query'
+
+import { EnvelopesQuery, components } from '../../src'
+import minimal from '../../acceptance/minimal/minimal.feature'
+
+const { QueriesWrapper, FilteredResults } = components.app
+
+export default {
+  title: 'FilteredResults',
+  component: components.app.FilteredResults,
+} as Meta
+
+function props(envelopes: readonly messages.Envelope[]): Props {
+  const gherkinQuery = new GherkinQuery()
+  const cucumberQuery = new CucumberQuery()
+  const envelopesQuery = new EnvelopesQuery()
+  for (const envelope of envelopes) {
+    gherkinQuery.update(envelope)
+    cucumberQuery.update(envelope)
+    envelopesQuery.update(envelope)
+  }
+  return { gherkinQuery, cucumberQuery, envelopesQuery }
+}
+
+type Props = {
+  gherkinQuery: GherkinQuery
+  cucumberQuery: CucumberQuery
+  envelopesQuery: EnvelopesQuery
+}
+
+type TemplateArgs = {
+  envelopes: readonly messages.Envelope[]
+}
+
+const Template: Story<TemplateArgs> = ({ envelopes }) => {
+  return (
+    <QueriesWrapper {...props(envelopes)}>
+      <FilteredResults />
+    </QueriesWrapper>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  envelopes: minimal,
+}
