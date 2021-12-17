5a193191c33f1594820260dfb4d0f534df79d6c7
David Goss <david@davidgoss.co>
2021-12-17 14:24:25 +0000

    add a story including ci

--- react/javascript/src/stories/ExecutionSummary.stories.tsx
+++ react/javascript/src/stories/ExecutionSummary.stories.tsx
@@ -16,6 +16,24 @@ const testData: messages.Meta = {
   ci: null,
 }
 
+const testDataWithCi: messages.Meta = {
+  protocolVersion: '17.1.1',
+  implementation: { version: '8.0.0-rc.1', name: 'cucumber-js' },
+  cpu: { name: 'x64' },
+  os: { name: 'linux', version: '5.11.0-1022-azure' },
+  runtime: { name: 'node.js', version: '16.13.1' },
+  ci: {
+    name: 'GitHub Actions',
+    url: 'https://github.com/cucumber/cucumber-js/actions/runs/1592557391',
+    buildNumber: '1592557391',
+    git: {
+      revision: 'b53d820504b31c8e4d44234dc5eaa58d6b7fdd4c',
+      remote: 'https://github.com/cucumber/cucumber-js.git',
+      branch: 'main',
+    },
+  },
+}
+
 export default {
   title: 'App/ExecutionSummary',
   component: components.app.ExecutionSummary,
@@ -32,4 +50,9 @@ const Template: Story<IExecutionSummaryProps> = (props) => {
 export const Default = Template.bind({})
 Default.args = {
   meta: testData,
+} as IExecutionSummaryProps
+
+export const WithCi = Template.bind({})
+WithCi.args = {
+  meta: testDataWithCi,
 } as IExecutionSummaryProps
