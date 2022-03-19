2d1fff6220d566b195345d84f294759ad69bb1ef
Ciaran McNulty <mail@ciaranmcnulty.com>
2022-03-19 09:24:27 +0000

    Operate directly on the string chars
    This seems better than unpacking to int and converting back

--- messages/php/src/Id/FallbackUuidGenerator.php
+++ messages/php/src/Id/FallbackUuidGenerator.php
@@ -7,26 +7,33 @@ namespace Cucumber\Messages\Id;
 final class FallbackUuidGenerator implements IdGenerator
 {
     /**
-     * Inspired by
-     * @see https://github.com/clue/reactphp-clickhouse
+     * Generates a v4 (random based) UUID
+     *
+     * @see https://datatracker.ietf.org/doc/html/rfc4122
      */
     public function newId(): string
     {
         $bytes = random_bytes(16);
 
         /**
-         * set MSBs of octet 6 to 0100... to indicate v4
-         *
-         * @see https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.3
-         */
-        $bytes[6] = chr(ord($bytes[6]) & 0b0000_1111 | 0b0100_0000);
-
-        /**
-         * set MSBs of octet 8 to 10... to indicate rfc4122
+         * set MSBs of octet 8 to 10xxxxxx. to indicate rfc4122 compliance
          *
          * @see https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.1
          */
-        $bytes[8] = chr(ord($bytes[8]) & 0b00_111111 | 0b10_000000);
+        $bytes[8] = $bytes[8]
+            & "\x3F" // 0 the first 2 bits
+            | "\x80" // raise 1st bit to 1
+        ;
+
+        /**
+         * set MSBs of octet 6 to 0100xxxx. to indicate v4
+         *
+         * @see https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.3
+         */
+        $bytes[6] = $bytes[6]
+            & "\x0F" // 0 the first 4 bits
+            | "\x40" // raise 2nd bit to 1
+        ;
 
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($bytes), 4));
     }
