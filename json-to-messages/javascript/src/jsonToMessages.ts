642a40aa3f522cf4961d0ddfaee5a05ce818236a
aurelien-reeves <aurelien.reeves@smartbear.com>
2022-04-25 12:07:48 +0200

    Update fake-cucumber to use ci-environment

--- json-to-messages/javascript/src/jsonToMessages.ts
+++ json-to-messages/javascript/src/jsonToMessages.ts
@@ -55,7 +55,6 @@ export default async function main(
   )
 
   const metaEnvelope: messages.Envelope = {
-    // '@cucumber/json-to-messages', version,
     meta: {
       protocolVersion: messages.version,
       implementation: {
