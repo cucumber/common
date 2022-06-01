3a5c38c83faca4474212ffffee4656cbea28d83c
Erik Huelsmann <ehuels@gmail.com>
2022-06-01 22:31:11 +0200

    [Perl] Align Makefile and default.mk more with Ruby
    By using the same build targets, where possible, and the same
    division of concerns between 'pre-release', 'test', 'clean' and
    'publish', the release process be more comprehensible for non-Perl
    contributors.

--- messages/perl/default.mk
+++ messages/perl/default.mk
@@ -3,7 +3,11 @@
 #     source scripts/functions.sh && rsync_files
 #
 SHELL := /usr/bin/env bash
-ALPINE := $(shell which apk 2> /dev/null)
+PERL_SOURCE_FILES = $(shell find lib -name '*.pm') $(shell test -d bin && find bin -type f) $(shell find t -name '*.t')
+LIBNAME := $(shell head -10 dist.ini | grep 'name.*=' | sed -e 's/name.*= *//')
+DISTINI  = $(shell find . -name 'dist.ini')
+DIST := $(LIBNAME)-$(NEW_VERSION).tar.gz
+IS_TESTDATA = $(findstring -testdata,${CURDIR})
 
 ### COMMON stuff for all platforms
 
@@ -18,20 +22,42 @@ endef
 
 ### Common targets for all functionalities implemented on Perl
 
-default: test
+default: .tested
 .PHONY: default
 
-CHANGELOG.md: ../CHANGELOG.md
-	cp ../CHANGELOG.md CHANGELOG.md
+.deps: .cpanfile_dependencies
+	touch $@
 
-distribution: predistribution
-	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil test --release
+.cpanfile_dependencies: cpanfile dist.ini
+	# --notest to keep the number of dependencies low: it doesn't install the
+	# testing dependencies of the dependencies.
+	cpanm --notest --local-lib ./perl5 --installdeps --with-develop .
+	cpanm --notest --local-lib ./perl5 'Dist::Zilla'
+	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil authordeps --missing | cpanm --notest --local-lib ./perl5
+	touch $@
+
+.tested: .deps $(PERL_SOURCE_FILES)
+	PERL5LIB=${PERL5LIB} prove -l
+	touch $@
+
+ifdef NEW_VERSION
+ifneq (,$(DISTINI))
+dist: $(DIST)
+else
+dist:
+	@echo "Not building dist because there is no dist.ini"
+endif
+endif
+.PHONY: dist
+
+$(DIST): .tested
+	@(git status --porcelain 2>/dev/null | grep "^??" | perl -ne\
+	    'die "The `release` target includes all files in the working directory. Please remove [$$_], or add it to .gitignore if it should be included\n" if s!.+ perl/(.+?)\n!$$1!')
 	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil build
-.PHONY: distribution
+	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} cpanm --local-lib $(shell mktemp -d) --test-only $(DIST)
 
-publish: predistribution
-	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil release
-.PHONY: publish
+pre-release: update-version CHANGELOG.md dist
+.PHONY: pre-release
 
 update-version:
 ifdef NEW_VERSION
@@ -42,28 +68,31 @@ else
 endif
 .PHONY: update-version
 
-.cpanfile_dependencies: cpanfile
-	PERL5LIB=${PERL5LIB} cpanm --notest --local-lib ./perl5 --installdeps .
-	touch $@
 
-predistribution: dist-clean test CHANGELOG.md
-# --notest to keep the number of dependencies low: it doesn't install the
-# testing dependencies of the dependencies.
-	cpanm --notest --local-lib ./perl5 --installdeps --with-develop .
-	cpanm --notest --local-lib ./perl5 'Dist::Zilla'
-	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil authordeps --missing | cpanm --notest --local-lib ./perl5
-	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil clean
-	@(git status --porcelain 2>/dev/null | grep "^??" | perl -ne\
-	    'die "The `release` target includes all files in the working directory. Please remove [$$_], or add it to .gitignore if it should be included\n" if s!.+ perl/(.+?)\n!$$1!')
-.PHONY: predistribution
-
-pre-release: update-version
-.PHONY: pre-release
+publish: dist
+ifeq ($(IS_TESTDATA),-testdata)
+	# no-op
+else
+ifneq (,$(DISTINI))
+	cpan-upload $(DIST)
+endif
+endif
+.PHONY: publish
 
 post-release:
 .PHONY: post-release
 
-dist-clean: clean
-	rm -rf ./perl5
+perl-clean:
+	PERL5LIB=${PERL5LIB} PATH=$$PATH:${PERL5PATH} dzil clean
+	rm -rf ./perl5 .deps .cpanfile_dependencies .tested
+.PHONY: perl-clean
 
+clean: perl-clean
+.PHONY: clean
+
+CHANGELOG.md: ../CHANGELOG.md
+	cp ../CHANGELOG.md CHANGELOG.md
+
+
+dist-clean: clean
 .PHONY: dist-clean
