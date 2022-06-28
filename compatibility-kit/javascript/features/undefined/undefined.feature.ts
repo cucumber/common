06d517dc1c9b8b3070ebacf5a9e1a35f3beae549
aurelien-reeves <aurelien.reeves@smartbear.com>
2022-06-28 11:17:54 +0200

    Remove ./compatibility-kit/

--- compatibility-kit/javascript/features/undefined/undefined.feature.ts
+++
@@ -1,9 +0,0 @@
-import { Given } from '@cucumber/fake-cucumber'
-
-Given('an implemented step', function () {
-  // no-op
-})
-
-Given('a step that we expect to be skipped', function () {
-  // no-op
-})
