12e8729a5e5edce691de5946be928ecaec09bf7d
temyers <temyers@users.noreply.github.com>
2022-10-24 11:41:36 +0800

    fix: backport f-string to python2 compatible implementation

--- gherkin/python/gherkin/token_matcher.py
+++ gherkin/python/gherkin/token_matcher.py
@@ -179,7 +179,7 @@ class GherkinInMarkdownTokenMatcher(object):
     def _match_title_line(self, prefix, keywords, keywordSuffix, token, token_type):
         
         keywords_or_list="|".join(map(lambda x: re.escape(x), keywords))
-        match = re.search(f'{prefix}({keywords_or_list}){keywordSuffix}(.*)', token.line.get_line_text())
+        match = re.search('{}({}){}(.*)'.format(prefix,keywords_or_list, keywordSuffix), token.line.get_line_text())
         indent = token.line.indent
         result = False
         
