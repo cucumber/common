ec9a174170ae9abf936e40b5e4f0702daf75346e
temyers <temyers@users.noreply.github.com>
2022-11-08 19:39:31 +0800

    gherkin/python: Support gherkin markdown (#2103)
    Ports the Javascript implementation of Gherkin Markdown support to Python.
    
    This implementation ports the specifications and implementation from the
    Javascript for a (hopefully) like-for-like implementation.

--- gherkin/python/gherkin/token_matcher.py
+++ gherkin/python/gherkin/token_matcher.py
@@ -3,6 +3,17 @@ from collections import defaultdict
 from .dialect import Dialect
 from .errors import NoSuchLanguageException
 
+# Source: https://stackoverflow.com/a/8348914
+try:
+    import textwrap
+    textwrap.indent
+except AttributeError:  # undefined function (wasn't added until Python 3.3)
+    def indent(text, amount, ch=' '):
+        padding = amount * ch
+        return ''.join(padding+line for line in text.splitlines(True))
+else:
+    def indent(text, amount, ch=' '):
+        return textwrap.indent(text, amount * ch)
 
 class TokenMatcher(object):
     LANGUAGE_RE = re.compile(r"^\s*#\s*language\s*:\s*([a-zA-Z\-_]+)\s*$")
